477539959230b7fd3a7871a3d8552f895d3baf48
Will Fisher <will@gina.alaska.edu>
2011-06-15 16:51:57 -0800

    use better names for the include functions

--- extras/gina/cdn/helpers.rb
+++ extras/gina/cdn/helpers.rb
@@ -5,22 +5,22 @@ module GINA
         File.join(::GINA::URL, 'libs')
       end
 
-      def js_include_path(name, version=nil)
+      def include_js_path(name, version=nil)
         lib = ::GINA::CDN::Lib.new(name, version)
         lib.js.collect { |f| File.join(path, f) }
       end
 
-      def js_include_tag(lib, *h)
-        javascript_include_tag(js_include_path(lib), *h)
+      def include_js(lib, *h)
+        javascript_include_tag(include_js_path(lib), *h)
       end
 
-      def css_link_path(name, version=nil)
+      def include_css_path(name, version=nil)
         lib = ::GINA::CDN::Lib.new(name, version)
         lib.css.collect { |f| File.join(path, f) }
       end
 
-      def css_link_tag(lib, *h)
-        stylesheet_link_tag(css_link_path(lib), *h)
+      def include_css(lib, *h)
+        stylesheet_link_tag(include_css_path(lib), *h)
       end
     end
   end
