58246b582060b82040f4abe9106f5dd7f91ba72b
Will Fisher <will@teknofire.net>
2011-09-19 17:15:34 -0800

    adding in the splash screen to the application

--- public/javascripts/app/view/catalog/splash.js
+++ public/javascripts/app/view/catalog/splash.js
@@ -3,43 +3,45 @@ Ext.define('App.view.catalog.splash', {
   alias: 'widget.catalog_splash',
   width: 725,
   height: 525,
-  layout: {
-    type: 'vbox',
-    align: 'stretch'
-  },
-  plain: true,
-  closable: false,
 
-  defaults: { border: false, cls: 'transparent-bg' },
-  cls: 'transparent-bg',
+	layout: { type: 'vbox', align: 'stretch' },
+	// layout: 'fit',
+	
+	modal: true,
+  // plain: true,
+  closable: false,
+	preventHeader: true,
+	// bodyBorder: false,
+
+  // defaults: { border: false, cls: 'transparent-bg' },
+  // cls: 'transparent-bg',
   items: [{
-    region: 'north',
+    dock: 'top',
+		border: false,
+		xtype: 'panel',
+		height: 80,
     contentEl: 'splash-header'
-  }, {
-    region: 'center',
+  },	{
     autoScroll: true,
     flex: 1,
-    contentEl: 'splash-content',
-    docked: [{
-      dock: 'bottom',
-      buttonAlign: 'center',
-      items: [{
-        scale: 'large',
-        text: 'Guest',
-        xtype: 'button',
-        handler: function(button) {
-        }
-      }, ' ',{
-        scale: 'large',
-        text: 'Login',
-        xtype: 'button',
-        handler: function() {
-        }
-      }]
-    }]
+		border: false,
+    contentEl: 'splash-content'
   }, {
-    region: 'south',
-    border: false,
+		xtype: 'panel',
+		border: false,
+		layout: { type: 'hbox', pack: 'center' },
+		defaults: { scale: 'large', width: 200, margin: '0 3 0 3' },
+		items: [
+			{ xtype: 'button', text: 'Guest', handler: function(button) { button.up('window').close(); } },
+			{ xtype: 'button', text: 'Login', handler: function(button) { button.up('window').close(); } }
+		]
+	},	{
+		dock: 'bottom',
+		xtype: 'panel',
+		border: false,
+		layout: 'fit',
+		height: 100,
     contentEl: 'splash-footer'
-  }]
+  }],
+	// items: [{ html: 'test', border: false, bodyBorder: false }]
 });
