a9b2efd0e4755ba70353ed41a5c5ca10898a798d
Will Fisher <will@gina.alaska.edu>
2011-09-26 16:02:51 -0800

    adding cms code for news and help

---
+++ vendor/cms/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NssiCms::Application.config.secret_token = 'ead78fa7bb370a520cb929f76e6cf11afc9011aa8950ef76ec38504df4d2411e5f4072a78890b046e82c372f8576cf6e4e3a74b5c58e96d6109c99f8f7f4c658'
