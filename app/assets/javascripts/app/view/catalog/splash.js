d3ee27fe5874a2dd087d6871a6021efe67552d9b
Will Fisher <will@teknofire.net>
2011-10-31 12:08:19 -0800

    new splash loading message handler

--- app/assets/javascripts/app/view/catalog/splash.js
+++ app/assets/javascripts/app/view/catalog/splash.js
@@ -2,7 +2,7 @@ Ext.define('App.view.catalog.splash', {
   extend: 'Ext.window.Window',
   alias: 'widget.catalog_splash',
   width: 725,
-  height: 525,
+  height: 500,
 
   layout: { type: 'vbox',align: 'stretch' },
 
@@ -22,17 +22,22 @@ Ext.define('App.view.catalog.splash', {
     border: false,
     contentEl: 'splash-content'
   },{
+    itemId: 'loading',
+    bodyStyle: 'text-align: center; font-size: 20px;',
+    xtype: 'panel',
+    border: false,
+    html: 'Please wait loading records... <img src="/assets/loading.gif" />'
+  }, {
+    itemId: 'buttons',
+    hidden: true,
     xtype: 'panel',
     border: false,
     layout: { type: 'hbox',pack: 'center' },
     defaults: { scale: 'large',width: 200, margin: '0 3 0 3' },
     items: [{
       xtype: 'button',
-      text: 'Guest',
-      handler: function(button) { button.up('window').close(); }
-    },{
-      xtype: 'button',
-      text: 'Login',
+      cls: 'enter-button',
+      text: 'Enter the Catalog',
       handler: function(button) { button.up('window').close(); }
     }]
   },{
@@ -42,5 +47,11 @@ Ext.define('App.view.catalog.splash', {
     layout: 'fit',
     height: 100,
     contentEl: 'splash-footer'
-  }]
+  }],
+  
+  pageReady: function() {
+    this.getComponent('loading').hide();
+    // this.getComponent('button-message').show();
+    this.getComponent('buttons').show();
+  }
 });
