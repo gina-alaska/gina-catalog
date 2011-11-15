b846de3901981fb648a35e97bbad5745f426402e
Will Fisher <will@teknofire.net>
2011-11-15 12:53:20 -0900

    update with new default text code

--- app/assets/javascripts/gina/DefaultText.js
+++ app/assets/javascripts/gina/DefaultText.js
@@ -3,43 +3,56 @@ Ext.define('Ext.gina.DefaultText', {
 
   initComponent: function() {
     this.addEvents('load');
-
     this.callParent();
   },
 
   setToDefault: function(input) {
-    if(input.getValue() == null || input.getValue() == '') { input.setValue(this.text); }
+    var v = input.getValue();
+    if(v === null || v == this.emptyValue || (Ext.isArray(v) && v.indexOf(this.emptyValue) >= 0)) { 
+      input.setValue('');
+      input.addCls('default');
+      return false;
+		}
   },
 
   init: function(field) {
-    var defaultText = this.text;
-
-    field.setValueWithoutDefault = field.setValue;
-    field.setValue = function(v) {
-      if(v == defaultText) {
-        this.addCls('default');
-      } else {
-        this.removeCls('default');
-      }
-      return this.setValueWithoutDefault(v);
-    }.bind(field);
-
-    field.getValueWithoutCheck = field.getValue;
-    field.getValue = function() {
-      var v = this.getValueWithoutCheck();
-      return (v == defaultText ? '' : v);
-    }.bind(field);
+		var me = this;
+		
+    this.emptyValue =  (this.emptyValue === null ? '' : this.emptyValue);
+		field.emptyText = this.text;
+		field.submitEmptyText = false;
 
     field.addCls('default');
 
     field.on('render', this.setToDefault, this, { delay: 100 });
 
     field.on('focus', function(input) {
-      if(input.getValueWithoutCheck() == defaultText) { input.setValue(''); }
+			input.removeCls('default');
+			if(input.getValue() == this.emptyValue) { input.setValue(''); }
     }, this);
 
     field.on('blur', this.setToDefault, this);
+    
+    /* 
+      If a user tells us to add an empty option and there is a store,
+      add it to the store based on the configured values
+    */
+    if(this.addEmptyChoice && field.store) {
+      var choice = {};
+      choice[field.valueField] = this.emptyValue;
+      choice[field.displayField] = this.text;
+      
+      field.store.on('load', function(store) {  
+        store.insert(0, choice);
+      });
+      field.store.insert(0, choice);
+    }
+    
+    /* If user selects blank option clear out all the other selections */
+    if(field.multiSelect) {
+      field.on('select', this.setToDefault, this);
+    }
 
-    this.fireEvent('load', this)
+    this.fireEvent('load', this);
   }
 });
