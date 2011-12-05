74821485c90ec06fea961c8a3779217ee719c4e1
Will Fisher <will@gina.alaska.edu>
2011-12-05 10:37:32 -0900

    change location of the link

--- config/deploy.rb
+++ config/deploy.rb
@@ -33,7 +33,7 @@ namespace :deploy do
   end
 
   task :link_configs do
-    run "ln -nfs #{deploy_to}/#{shared_dir}/system/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{deploy_to}/#{shared_dir}/config/database.yml #{release_path}/config/database.yml"
   end
   task :precompile_assets do
     run "cd #{release_path}; RAILS_ENV=production rake assets:precompile"
