c3e808665d1de33dbf51392745639546a4d911c3
Will Fisher <will@teknofire.net>
2011-12-12 10:28:02 -0900

    Adding support for new video

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -3,7 +3,17 @@ class VideosController < ApplicationController
 
   def show
     @video = Video.find_by_name(params[:id])
-
-    respond_with(@video)
+    # if(!@video) 
+    #   @video = Video.create(
+    #     name: 'quickstart2x', filename: 'catalog2x_controller.swf', 
+    #     path: '/video/catalog2x', title: 'Quick Start'
+    #   )  
+    # end
+    
+    if !@video.nil? and @video.exists?
+      respond_with(@video)
+    else
+      render :file => "#{Rails.root}/public/404.html", :status => 404, :layout => false
+    end
   end
 end
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,9 @@
 class Video < ActiveRecord::Base
+  def fullpath
+    File.join(path, filename)
+  end
+  
+  def exists?
+    File.exists? fullpath
+  end
 end
