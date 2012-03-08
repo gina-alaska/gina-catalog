9649ff51df89aea55477d5656fb80ce3064a49c5
Will Fisher <will@teknofire.net>
2012-03-07 19:44:20 -0900

    fixing validation messages

--- app/models/link.rb
+++ app/models/link.rb
@@ -4,6 +4,6 @@ class Link < ActiveRecord::Base
   belongs_to :asset, :polymorphic => true
 
   validates_length_of :display_text, :in => 3..255
-  validates_length_of :url, :in => 11..255
-  validates_inclusion_of :category, :in => CATEGORIES, :message => "{{value}} is not a valid category"
+  validates_length_of :url, :in => 11..255, :message => "%{value} is not a valid url"
+  validates_inclusion_of :category, :in => CATEGORIES, :message => "%{value} is not a valid category"
 end
