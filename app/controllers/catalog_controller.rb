ff616b96119ed314cc5444828ed5d969dbcc4f97
Will Fisher <will@teknofire.net>
2012-03-19 11:30:46 -0800

    don't mess with location information for now

--- app/controllers/catalog_controller.rb
+++ app/controllers/catalog_controller.rb
@@ -185,7 +185,7 @@ class CatalogController < ApplicationController
   def catalog
     v = params.slice(
           :title, :description, :agengy_ids, :tags, :source_agency_id, :status,
-          :geokeyword_ids, :links_attributes, :locations_attributes,
+          :geokeyword_ids, :links_attributes,
           :agency_ids, :person_ids, :iso_topic_ids, :start_date, :end_date, :long_term_monitoring, 
     )    
     v["tags"] = v["tags"].split(/,\s+/)
