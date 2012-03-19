53fe74fbbb4e204eb62db4f7da2c090c9d41fe37
Will Fisher <will@teknofire.net>
2012-03-19 12:18:59 -0800

    showing longer description of the iso theme words

--- app/models/iso_topic.rb
+++ app/models/iso_topic.rb
@@ -1,3 +1,14 @@
 class IsoTopic < ActiveRecord::Base
   set_table_name 'iso_topic_categories'
+  
+  def long_name_with_code
+    "#{iso_theme_code} :: #{name.humanize} - #{long_name}"
+  end
+  
+  def as_json(*opts)
+    hash = super
+    hash[:long_name_with_code] = long_name_with_code
+    
+    hash
+  end
 end
