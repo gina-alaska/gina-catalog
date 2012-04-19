e5a31528faa07579dd496a41a3b6d060d998b7ed
Will Fisher <will@teknofire.net>
2012-04-19 14:43:20 -0800

    adding auth configs to production environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,35 @@
 NSCatalog::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  Rails.application.config.middleware.use OmniAuth::Builder do
+   # ALWAYS RESTART YOUR SERVER IF YOU MAKE CHANGES TO THESE SETTINGS!
+   
+   # you need a store for OpenID; (if you deploy on heroku you need Filesystem.new('./tmp') instead of Filesystem.new('/tmp'))
+   require 'openid/store/filesystem'
+   
+   # providers with id/secret, you need to sign up for their services (see below) and enter the parameters here
+   # provider :facebook, 'APP_ID', 'APP_SECRET'
+   # provider :twitter, 'CONSUMER_KEY', 'CONSUMER_SECRET'
+   # provider :github, 'CLIENT ID', 'SECRET'
+   
+   # generic openid
+   # provider :openid, :store => OpenID::Store::Filesystem.new('./tmp'), :name => 'openid'
+   
+   # dedicated openid
+   provider :openid, :store => OpenID::Store::Filesystem.new('/tmp'), :name => 'gina', :identifier => 'http://id.gina.alaska.edu'
+   # provider :google_apps, OpenID::Store::Filesystem.new('./tmp'), :name => 'google_apps'
+   # /auth/google_apps; you can bypass the prompt for the domain with /auth/google_apps?domain=somedomain.com
+   
+   # provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'yahoo', :identifier => 'yahoo.com' 
+   # provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'aol', :identifier => 'openid.aol.com'
+   # provider :openid, OpenID::Store::Filesystem.new('./tmp'), :name => 'myopenid', :identifier => 'myopenid.com'
+
+   # Sign-up urls for Facebook, Twitter, and Github
+   # https://developers.facebook.com/setup
+   # https://github.com/account/applications/new
+   # https://developer.twitter.com/apps/new
+  end  
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
@@ -43,7 +72,7 @@ NSCatalog::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( prototype.js manager.js print.css ie.css pdf.css )
+  config.assets.precompile += %w( prototype.js application.js manager.js print.css ie.css pdf.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
