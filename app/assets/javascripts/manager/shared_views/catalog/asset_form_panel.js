4e251e780dc7eafc9d7ad53f325579a7e0369262
Will Fisher <will@teknofire.net>
2012-05-29 10:43:28 -0800

    fixing description field height

--- app/assets/javascripts/manager/shared_views/catalog/asset_form_panel.js
+++ app/assets/javascripts/manager/shared_views/catalog/asset_form_panel.js
@@ -4,19 +4,19 @@
 * General form fields
 */
 Ext.define('Manager.shared_views.catalog.asset_form_panel', {
-extend: 'Ext.panel.Panel', 
-alias: 'widget.catalog_asset_form_panel',
+	extend: 'Ext.panel.Panel', 
+	alias: 'widget.catalog_asset_form_panel',
 
-initComponent: function() {
-  Ext.apply(this, {
-    layout: 'anchor',
-    items: [
-      { xtype: 'hiddenfield', name: 'type', value: 'Asset' },
-      { xtype: 'textfield', fieldLabel: 'Title', name: 'title' }, 
-      { xtype: 'textarea', fieldLabel: 'Description', name: 'description' }
-    ]
-  });        
+	initComponent: function() {
+	  Ext.apply(this, {
+	    layout: 'anchor',
+	    items: [
+	      { xtype: 'hiddenfield', name: 'type', value: 'Asset' },
+	      { xtype: 'textfield', fieldLabel: 'Title', name: 'title' }, 
+	      { xtype: 'textarea', fieldLabel: 'Description', name: 'description', height: 180 }
+	    ]
+	  });        
 
-  this.callParent(arguments);
-}
+	  this.callParent(arguments);
+	}
 });
