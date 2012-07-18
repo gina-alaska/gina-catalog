06ae77e6a24818b6c8db9ee792e4f6555ac80cf9
Will Fisher <will@teknofire.net>
2012-07-18 12:29:13 -0800

    fixing default image for gravatar
    [#1]

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def avatar_url(user, size=48)
-    default_url = "#{root_url}images/guest.png"
+    default_url = "mm"
     gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI.escape(default_url)}"
   end
