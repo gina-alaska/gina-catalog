08773c782bfc59bd5aaa64aa14923e198c52c89c
Will Fisher <will@teknofire.net>
2012-08-30 15:47:56 -0800

    left out the admins!

--- app/controllers/sds_admin/contact_infos_controller.rb
+++ app/controllers/sds_admin/contact_infos_controller.rb
@@ -2,7 +2,7 @@ class SdsAdmin::ContactInfosController < SdsAdminController
   respond_to :html
   
   def index
-    if false and current_user.admin?
+    if current_user.admin?
       @contact_infos = ContactInfo
     elsif current_user.agency
       @contact_infos = ContactInfo.joins(:catalog).where(:catalog => { :source_agency_id => current_user.agency.id })
