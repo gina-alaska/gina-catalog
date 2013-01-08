19eab1edbfacc7502b1d720d60994b1cbb4189b5
Will Fisher <will@teknofire.net>
2013-01-08 10:04:05 -0900

    fixing markitup so it now shows up

---
+++ app/assets/javascripts/manager.js
@@ -0,0 +1,11 @@
+//= require 'jquery'
+//= require 'jquery_ujs'
+//= require 'jquery.markitup'
+//= require 'jquery.slugify'
+//= require 'bootstrap'
+//= require 'jquery-file-upload/jquery.ui.widget'
+//= require 'jquery-file-upload/jquery.fileupload'
+//= require 'jquery-file-upload/jquery.iframe-transport'
+//= require 'jquery-file-upload/tmpl.min'
+//= require 'markitup'
+//= require_self
