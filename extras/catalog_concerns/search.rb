cca37e664fdba1567da74a7b3c4558b67c58488a
Scott Macfarlane <macfarlane.scott@gmail.com>
2013-01-12 11:30:51 -0900

    Primary and Funding Agency search should be singular

--- extras/catalog_concerns/search.rb
+++ extras/catalog_concerns/search.rb
@@ -46,8 +46,8 @@ module CatalogConcerns
           with :archived_at_year, nil unless search[:archived]
           with :type, search[:type] if search[:type].present?
           with :agency_ids, search[:agency_ids] if search[:agency_ids].present?
-          with :source_agency_id, search[:source_agency_ids] if search[:source_agency_ids].present?
-          with :funding_agency_id, search[:funding_agency_ids] if search[:funding_agency_ids ].present?
+          with :source_agency_id, search[:source_agency_id] if search[:source_agency_id].present?
+          with :funding_agency_id, search[:funding_agency_id] if search[:funding_agency_id].present?
           with :iso_topic_ids, search[:iso_topic_ids] if search[:iso_topic_ids].present?
           with :primary_contact_id, search[:primary_contact_id] if search[:primary_contact_id].present?
           with :person_ids, search[:contact_ids] if search[:contact_ids].present?
