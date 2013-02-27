b60c3dcae35acefb05842fdbde543ad177e868b7
grimm <jason@gina.alaska.edu>
2013-02-27 13:03:17 -0900

    Adding in a datepicker for the start_date and end_date on the manager record edit page.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -8,7 +8,6 @@
 //= require 'jquery-file-upload/jquery.iframe-transport'
 //= require 'jquery-file-upload/tmpl.min'
 //= require 'select2/select2.min'
-//= require bootstrap-datepicker
 //= require_self
 
 $(document).ready(function(){
