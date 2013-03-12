5742951f9e9d3dbd5f7453409a11e887fc79f281
Will Fisher <will@teknofire.net>
2013-03-12 12:24:47 -0800

    lots of changes for permission handling
    Implemented permission tables with role and permission relations

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130307204627) do
+ActiveRecord::Schema.define(:version => 20130312201213) do
 
   create_table "abstracts", :force => true do |t|
     t.string   "project_id"
@@ -590,6 +590,7 @@ ActiveRecord::Schema.define(:version => 20130307204627) do
     t.string   "description"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "group"
   end
 
   create_table "persons_setups", :id => false, :force => true do |t|
