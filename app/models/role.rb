3ac4c417ee83fb2641dcd14ef4621bd199cd986c
Will Fisher <will@teknofire.net>
2013-03-13 12:26:56 -0800

    easier method for saving checked permissions

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,7 +1,9 @@
 class Role < ActiveRecord::Base
+  attr_accessible :permission_ids, :name, :description
+  
   has_many :user_roles
   has_many :users, :through => :user_roles
-  has_many :role_permissions
+  has_many :role_permissions, uniq: true
   has_many :permissions, :through => :role_permissions
   has_many :membership_roles
   has_many :memberships, :through => :membership_roles
