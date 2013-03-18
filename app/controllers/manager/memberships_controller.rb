9d724a99bbd17df9a6652d7c92ff8e622b84960f
grimm <jason@gina.alaska.edu>
2013-03-18 10:20:31 -0800

    Allows admin to invite new users to site.

---
+++ app/controllers/manager/memberships_controller.rb
@@ -0,0 +1,71 @@
+class Manager::MembershipsController < ManagerController
+	def index
+    @memberships = current_setup.memberships.all
+  end
+
+  def new
+    @membership = Membership.new(email: params["email"])
+  end
+
+  def create
+    @membership = current_setup.memberships.build(membership_params)
+    
+    respond_to do |format|
+      if @membership.save
+        format.html do
+          flash[:success] = "Created new membership for #{@membership.email}."
+          redirect_to manager_memberships_path
+        end
+      else
+        format.html do
+          flash[:error] = 'Error while trying to create membership'
+          render 'new'
+        end
+      end
+    end
+  end
+
+  def edit
+    @membership = current_setup.memberships.find(params[:id])
+  end
+
+  def update
+    @membership = current_setup.memberships.find(params[:id])
+    
+    respond_to do |format|
+      if @membership.update_attributes(membership_params)
+        format.html do
+          flash[:success] = "Updated membership for #{@membership.email}"
+          redirect_to manager_memberships_path
+        end
+      else
+        format.html do
+          flash[:error] = 'Error while trying to update membership'
+          render 'edit'
+        end
+      end
+    end
+  end
+
+  def destroy
+    @membership = current_setup.memberships.find(params[:id])
+    
+    if @membership.destroy
+      flash[:success] = "Deleted membership for #{@membership.email}"
+    else        
+      flash[:error] = "Error while trying to delete membership"
+    end
+    
+    respond_to do |format|
+      format.html do
+        redirect_to manager_memberships_path
+      end
+    end
+  end
+
+  protected
+  
+  def membership_params
+    mparams = params[:membership].slice(:email, :role_ids)
+  end
+end
