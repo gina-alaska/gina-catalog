65e8426a9628567b8163e4572fd259d17b0ea275
grimm <jason@gina.alaska.edu>
2013-03-18 17:55:22 -0800

    Added authenticate manage_cms to the CMS models

--- app/controllers/manager/contacts_controller.rb
+++ app/controllers/manager/contacts_controller.rb
@@ -1,5 +1,6 @@
 class Manager::ContactsController < ManagerController
   before_filter :authenticate_manage_catalog!
+  before_filter :authenticate_manage_cms!
 
   def index
     @contacts = current_setup.contacts
