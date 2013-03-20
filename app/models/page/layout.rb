b355e816ea832a07275297e47654a8e29022bd3f
Will Fisher <will@teknofire.net>
2013-03-20 11:28:00 -0800

    really fix this this time

--- app/models/page/layout.rb
+++ app/models/page/layout.rb
@@ -1,5 +1,5 @@
 class Page::Layout < ActiveRecord::Base
   attr_accessible :content, :name, :default
   
-  has_and_belongs_to_many :setups, class_name: 'Page::Layout', join_table: 'page_layouts_setups'
+  has_and_belongs_to_many :setups, join_table: 'page_layouts_setups'
 end
