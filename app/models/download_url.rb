866231c6d0b929c85311a424ec670486783f68c5
grimm <jason@gina.alaska.edu>
2013-03-26 14:52:58 -0800

    Small fixes on validation and reject_if's.

--- app/models/download_url.rb
+++ app/models/download_url.rb
@@ -3,6 +3,8 @@ class DownloadUrl < ActiveRecord::Base
   
   before_save :update_uuid
   
+  validates_presence_of :url
+
   def update_uuid
     self.uuid = UUIDTools::UUID.md5_create(UUIDTools::UUID_URL_NAMESPACE, self.url).to_s
   end
