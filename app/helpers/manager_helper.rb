79176acd0257838b086f2371e10ca110f653ef3c
Will Fisher <will@teknofire.net>
2013-04-15 18:17:04 -0800

    Initial commit with all the new ace stuff
    Still need to finish up the ajax saving

--- app/helpers/manager_helper.rb
+++ app/helpers/manager_helper.rb
@@ -1,2 +1,19 @@
 module ManagerHelper
+  def awesome_set_options(class_or_item, value_field = :id, mover = nil)
+    if class_or_item.is_a? Array
+      items = class_or_item.reject { |e| !e.root? }
+    else
+      class_or_item = class_or_item.roots if class_or_item.respond_to?(:scoped)
+      items = Array(class_or_item)
+    end
+    result = []
+    items.each do |root|
+      result += root.class.associate_parents(root.self_and_descendants).map do |i|
+        if mover.nil? || mover.new_record? || mover.move_possible?(i)
+          [yield(i), i.send(value_field)]
+        end
+      end.compact
+    end
+    result
+  end
 end
