030e0496d8977cb3a6f726f810df7078bd4efd24
grimm <jason@gina.alaska.edu>
2013-04-24 16:32:33 -0800

    Moved the new pagination controls from the public catalog page to the manager catalog page.

--- app/controllers/manager/catalogs_controller.rb
+++ app/controllers/manager/catalogs_controller.rb
@@ -16,13 +16,17 @@ class Manager::CatalogsController < ManagerController
   def index
     @page = params[:page] || 1
     @limit = 50
+    @pagenum = params[:page] || 1
+
     @search = params[:search] || {}
     @search[:order_by] ||= 'title_sort-ascending'
     if @search.keys.count > 0
       search = solr_search(@search, @page, @limit, true)
       @catalogs = search.results
+      @total = search.total
     else
       @catalogs = Catalog.order('title ASC').page(@page).per(@limit)
+      @total = Catalog.all.count
     end
     
     respond_with @catalogs
