5f47eb3fba5ff3a6fbf198465e28ffd4c9d8b9d8
grimm <jason@gina.alaska.edu>
2013-04-30 10:03:16 -0800

    Adding scope to model for date query, also re-formatted search form.

--- app/controllers/manager/contact_infos_controller.rb
+++ app/controllers/manager/contact_infos_controller.rb
@@ -7,17 +7,11 @@ class Manager::ContactInfosController < ManagerController
     @contact_infos = ContactInfo
     @page = params["page"].nil? ? 1 : params["page"]
     @limit = params["limit"].nil? ? 30 : params["limit"]
-    @total = @contact_infos.count
     @start_date = params["start_date"]
     @end_date = params["end_date"]
 
-    @end_date = Time.now.to_date if @end_date.blank? and !@start_date.blank?
-
-    if @start_date.blank?
-      @contact_infos = @contact_infos.order("created_at DESC").page(@page).per(@limit)
-    else
-      @contact_infos = @contact_infos.where("created_at >= ? AND created_at <= ?", @start_date, @end_date).order("created_at DESC").page(@page).per(@limit)
-    end
+    @total = @contact_infos.created_between(@start_date, @end_date).count
+    @contact_infos = @contact_infos.created_between(@start_date, @end_date).order("created_at DESC").page(@page).per(@limit)
 
     respond_to do |format|
       format.html
--- app/models/contact_info.rb
+++ app/models/contact_info.rb
@@ -4,4 +4,20 @@ class ContactInfo < ActiveRecord::Base
   validates_presence_of :name
   validates_presence_of :email
   validates_presence_of :usage_description
+
+  scope :created_between,  ->(starts_at, ends_at) {
+    unless starts_at.blank? and ends_at.blank?
+      data = self
+      unless starts_at.blank?
+        data = data.where("created_at >= ?", starts_at)
+      end
+      unless ends_at.blank?
+        data = data.where("created_at <= ?", ends_at)
+      end
+
+      data
+    else 
+      where("created_at <= ?", Time.now)
+    end
+  }
 end
