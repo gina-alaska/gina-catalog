0d3f0eac9a5c1b44093779290f9853637da37b6d
Will Fisher <will@teknofire.net>
2013-05-29 10:50:03 -0800

    adding downloads.js anc css files to precompile list

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ NSCatalog::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( application.js manager.js pdf.css manager.css admin.css font-awesome-ie7.min.css )
+  config.assets.precompile += %w( application.js manager.js pdf.css manager.css admin.css downloads.js downloads.css font-awesome-ie7.min.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
