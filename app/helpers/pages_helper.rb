9a852ac8611bb6734168237a27474bf20370bd9a
Will Fisher <will@teknofire.net>
2013-06-12 11:09:44 -0800

    adding support for new system liquid tag

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,6 +1,7 @@
 module PagesHelper  
-  def render_cms_content(page, setup)
-    CmsRenderers.page(page, setup)
+  def render_cms_content(page, setup, &block)
+    system_content = capture(&block) if block_given?
+    CmsRenderers.page(page, setup, system_content)
   end
   
   def render_snippet_content(snippet, setup)
--- extras/liquid_filter.rb
+++ extras/liquid_filter.rb
@@ -1,5 +1,5 @@
 class LiquidFilter < HTML::Pipeline::Filter
   def call
-    Liquid::Template.parse(@html).render('page' => context[:page], 'setup' => context[:setup], 'snippets' => context[:snippets])
+    Liquid::Template.parse(@html).render('page' => context[:page], 'setup' => context[:setup], 'snippets' => context[:snippets], 'system_content' => context[:system_content])
   end
 end
