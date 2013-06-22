89872deff9e79cbcecd7a127c166446029546672
grimm <jason@gina.alaska.edu>
2013-06-21 17:16:41 -0800

    The advanced catalog search now includes child setup agencies in the select lists.

--- app/helpers/agencies_helper.rb
+++ app/helpers/agencies_helper.rb
@@ -1,2 +1,17 @@
 module AgenciesHelper
+  def collection_select_for_agencies(name, selected, special)
+    search = Agency.search do
+      with :setup_ids, current_setup.self_and_descendants.pluck(:id)
+      paginate per_page:(5000)
+      order_by(:name, "asc")
+    end
+
+    options = options_from_collection_for_select(search.results, :id, :name_with_acronym, selected)
+
+    if special
+      select_tag(name, options,  class: 'span12', data: {placeholder: 'All Agencies', ui: 'select2'}, multiple: true, :include_blank => true)
+    else
+      select_tag(name, options,  class: 'span12', data: {placeholder: 'All Agencies', ui: 'select2'}, :include_blank => true)
+    end
+  end
 end
