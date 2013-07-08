635bcca6da491c9d1b9d7ccc1c97b171434f0822
grimm <jason@gina.alaska.edu>
2013-07-08 15:38:36 -0800

    Added agency filter to dashboard page.

--- app/helpers/agencies_helper.rb
+++ app/helpers/agencies_helper.rb
@@ -1,17 +1,18 @@
 module AgenciesHelper
-  def collection_select_for_agencies(name, selected, special)
+  def collection_select_for_agencies(name, selected, special, *css_class)
     search = Agency.search do
       with :setup_ids, current_setup.self_and_descendants.pluck(:id)
       paginate per_page:(5000)
       order_by(:name, "asc")
     end
 
+    css_class = "span12" if css_class.blank?
     options = options_from_collection_for_select(search.results, :id, :name_with_acronym, selected)
 
     if special
-      select_tag(name, options,  class: 'span12', data: {placeholder: 'All Agencies', ui: 'select2'}, multiple: true, :include_blank => true)
+      select_tag(name, options,  class: css_class, data: {placeholder: 'All Agencies', ui: 'select2'}, multiple: true, :include_blank => true)
     else
-      select_tag(name, options,  class: 'span12', data: {placeholder: 'All Agencies', ui: 'select2'}, :include_blank => true)
+      select_tag(name, options,  class: css_class, data: {placeholder: 'All Agencies', ui: 'select2'}, :include_blank => true)
     end
   end
 end
