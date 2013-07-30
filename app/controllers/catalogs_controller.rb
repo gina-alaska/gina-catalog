e801824c8fb22b8baa4b3845deed6c7b03b6779e
grimm <jason@gina.alaska.edu>
2013-07-29 16:11:21 -0800

    The CSV search download is now limited to a max of 150 rows.

--- app/controllers/catalogs_controller.rb
+++ app/controllers/catalogs_controller.rb
@@ -32,7 +32,7 @@ class CatalogsController < ApplicationController
     @search_params = params[:search] || {}
     @format = params[:format] || ""
     @limit = params[:limit] || 30
-    @limit = 30000 if @format == "csv"
+    @limit = 150 if @format == "csv"
     @pagenum = params[:page] || 1
 
     advanced_opts = @search_params.reject { |k,v| v.blank? or ['q', 'collection_id', 'order_by'].include?(k) }
