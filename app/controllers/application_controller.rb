5d4e46dfa39665abacdc3c44540c1a4218b028ca
Will Fisher <will@teknofire.net>
2013-07-31 13:51:18 -0800

    ability to cache certain permissions for portal memberships

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,9 +15,20 @@ class ApplicationController < ActionController::Base
   end
   alias_method :current_setup, :fetch_setup
 
-  def member_portals
+  def member_portals(permission = nil)
     return [] unless user_signed_in?
-    @member_portals ||= current_user.memberships.collect(&:setup).sort {|a,b| a.try(:title).to_s <=> b.try(:title).to_s }
+    # .sort {|a,b| a.try(:title).to_s <=> b.try(:title).to_s }
+    @member_portals ||= {}
+    @member_portals[:all] ||= current_user.setups.order('title ASC')
+    
+    if permission.nil?
+      return @member_portals[:all]
+    else
+      @member_portals[permission.to_sym] ||= @member_portals[:all].reject do |p|
+        !current_user.memberships.where(setup_id: p.id).first.send(permission)
+      end
+      return @member_portals[permission.to_sym]
+    end
   end
   helper_method :member_portals
   
@@ -28,7 +39,7 @@ class ApplicationController < ActionController::Base
   end
   
   def current_member
-    @current_member ||= current_user.memberships.where(setup_id: current_setup).includes(:setup).first || Membership.new(user: current_user) if user_signed_in?
+    @current_member ||= current_user.memberships.where(setup_id: current_setup).includes(:setup, :roles, :permissions).first || Membership.new(user: current_user) if user_signed_in?
   end
   
   def user_signed_in?
