e5d0592a3bef1d04a1eb275cdb2a7344e7a79815
Scott Macfarlane <macfarlane.scott@gmail.com>
2013-08-06 16:31:01 -0800

    Add some helper methods for importing from CSW

--- app/models/csw_import.rb
+++ app/models/csw_import.rb
@@ -1,6 +1,23 @@
 class CswImport < ActiveRecord::Base
-  attr_accessible :title, :sync_frequency, :url
+  attr_accessible :title, :sync_frequency, :url, :metadata_field, :metadata_type
   
   belongs_to :setup
   has_many :catalogs
+   
+  METADATA_TYPES = [
+    #['display text','type']
+    ['FGDC', 'FGDC']
+  ]
+  
+  def fgdc_import_url(record)
+    record.links.select{|l| l.type == self.metadata_field }.first.try(:url)
+  end
+  
+  def default_attributes
+    {
+      owner_setup_id: self.setup_id
+      #sds_use_agreements
+    }
+  end
+  
 end
