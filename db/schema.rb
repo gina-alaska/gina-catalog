66c516d099af1f681f8d572659e453b23e4964e2
grimm <jason@gina.alaska.edu>
2013-08-13 12:54:21 -0800

    Added google_layers boolean to setup model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130812204217) do
+ActiveRecord::Schema.define(:version => 20130813180647) do
 
   create_table "abstracts", :force => true do |t|
     t.string   "project_id"
@@ -790,8 +790,8 @@ ActiveRecord::Schema.define(:version => 20130812204217) do
     t.string   "by_line"
     t.string   "url"
     t.string   "logo_uid"
-    t.datetime "created_at",        :null => false
-    t.datetime "updated_at",        :null => false
+    t.datetime "created_at",                          :null => false
+    t.datetime "updated_at",                          :null => false
     t.string   "contact_email"
     t.text     "default_invite"
     t.text     "analytics_account"
@@ -807,6 +807,7 @@ ActiveRecord::Schema.define(:version => 20130812204217) do
     t.text     "description"
     t.text     "keywords"
     t.string   "projection"
+    t.boolean  "google_layers",     :default => true
   end
 
   create_table "setups_snippets", :id => false, :force => true do |t|
