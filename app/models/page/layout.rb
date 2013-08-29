404d54c9040d64e8ffb8202e78c7fa3913fc2a93
grimm <jason@gina.alaska.edu>
2013-08-29 10:33:18 -0800

    Added default before_destroy check and method.

--- app/models/page/layout.rb
+++ app/models/page/layout.rb
@@ -1,5 +1,11 @@
 class Page::Layout < ActiveRecord::Base
   attr_accessible :content, :name, :default
+
+  before_destroy :default?
   
   has_and_belongs_to_many :setups, join_table: 'page_layouts_setups'
+
+  def default?
+    !self.default 
+  end
 end
