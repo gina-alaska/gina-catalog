3ec838394a4e3434f4dea550a5b7534c2c72e6ba
grimm <jason@gina.alaska.edu>
2013-09-03 15:21:21 -0800

    Small fix for default layout logic.

--- app/models/page/layout.rb
+++ app/models/page/layout.rb
@@ -1,14 +1,14 @@
 class Page::Layout < ActiveRecord::Base
   attr_accessible :content, :name, :default
 
-  before_destroy :default?
+  before_destroy :default_check
   
   has_and_belongs_to_many :setups, join_table: 'page_layouts_setups'
   belongs_to :setup
 
   validates_uniqueness_of :name, scope: :setup_id
 
-  def default?
-    !self.default 
+  def default_check
+    !self.default? 
   end
 end
