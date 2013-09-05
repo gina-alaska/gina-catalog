8a912c02c6fef863bd529bfcfec2f7a5a463164f
grimm <jason@gina.alaska.edu>
2013-09-04 16:15:09 -0800

    Added system_pages to snippets.

--- app/models/page/snippet.rb
+++ app/models/page/snippet.rb
@@ -1,5 +1,10 @@
 class Page::Snippet < ActiveRecord::Base
   attr_accessible :content, :slug
+  before_destroy :prevent_system_delete
   
   validates_presence_of :slug
+
+  def prevent_system_delete
+    !self.system_page?
+  end
 end
