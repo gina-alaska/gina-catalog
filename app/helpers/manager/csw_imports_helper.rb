ebc35342566a89662f0d56741eec46a86e3caa21
Scott Macfarlane <scott@gina.alaska.edu>
2013-09-16 11:38:35 -0800

    Update views of csw logs

--- app/helpers/manager/csw_imports_helper.rb
+++ app/helpers/manager/csw_imports_helper.rb
@@ -1,18 +1,9 @@
 module Manager::CswImportsHelper
-  def humanize_import_error(error, value)
-    error_text = case error
-    when :start_date
-      "Unable to parse start date"
-    when :end_date
-      "Unable to parse end date"
-    when :agencies
-      "Unknown agency"
-    when :status
-      "Unknown status"
+  def import_status_text(status)
+    if status == "Finished"
+      "Schedule Import"
     else
-      "Unknown error"
+      status
     end
-    
-    content_tag(:dt, error_text) + content_tag(:dd, Array(value).first)
   end
 end
