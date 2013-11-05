5c87fd0e19668e9a5bea12b4cde2ffaddc3c2c23
Will Fisher <will@teknofire.net>
2013-11-05 14:11:59 -0900

    attempting to fix issues with bootstraping

--- Berksfile
+++ Berksfile
@@ -3,6 +3,8 @@ group :integration do
   cookbook 'minitest-handler'
 end
 
+cookbook "nginx"
+cookbook "application_ruby"
 cookbook "gina", "~> 0.3.6", chef_api: :config
 cookbook "postgresql", chef_api: :config
 cookbook "gina-postgresql", "~> 0.3.2", chef_api: :config
@@ -10,10 +12,6 @@ cookbook "gina-postgresql", "~> 0.3.2", chef_api: :config
 # cookbook "gina-webapp", path: "/Users/scott/workspace/cookbooks/gina-webapp" #chef_api: :config
 
 cookbook "chruby"
-cookbook "runit"
-cookbook "application"
-cookbook "application_ruby"
-cookbook "application_nginx"
 cookbook "user", git: 'http://github.com/fnichol/chef-user'
 
 metadata
--- metadata.rb
+++ metadata.rb
@@ -1,21 +1,19 @@
 name             'glynx'
-maintainer       'YOUR_NAME'
-maintainer_email 'YOUR_EMAIL'
+maintainer       'Will Fisher'
+maintainer_email 'will@gina.alaska.edu'
 license          'All rights reserved'
 description      'Installs/Configures glynx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.0'
+version          '1.0.3'
 
 supports "centos", ">= 6.0"
 
-depends "application"
-depends "application_ruby"
-depends "application_nginx"
 depends "chruby"
-depends "runit"
 
 depends "user"
+depends "application_ruby"
 depends "gina", "~> 0.3.6"
 depends "postgresql"
 depends "gina-postgresql"
+depends "nginx"
 # depends "gina-webapp"
