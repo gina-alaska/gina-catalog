6aa9c1d9889bb3719d7c8f32844741c59714fec1
Will Fisher <will@teknofire.net>
2013-12-03 17:16:50 -0900

    bumping version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'will@gina.alaska.edu'
 license          'All rights reserved'
 description      'Installs/Configures glynx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.9'
+version          '1.0.10'
 
 supports "centos", ">= 6.0"
 
