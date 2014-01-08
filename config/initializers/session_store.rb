c2531b2a66ccf4e9d38807fa4b2bbc28a31cd571
Will Fisher <will@teknofire.net>
2014-01-07 19:58:50 -0900

    add domain to session cookie

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,7 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
 # NSCatalog::Application.config.session_store :cookie_store, key: '_ns_catalog_session'
-Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 1.year
+Rails.application.config.session_store ActionDispatch::Session::CacheStore, :expire_after => 1.year, :domain => :all
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
