e3fb353d52013e94479f54d5a06ca56e39db1bdf
grimm <jason@gina.alaska.edu>
2014-01-28 17:37:48 -0900

    Allow catalog record links without a description, also fixed the tumblr icon.

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,7 +3,7 @@ class Link < ActiveRecord::Base
 
   belongs_to :asset, polymorphic: true
 
-  validates_length_of :display_text, in: 3..255
+  validates_length_of :display_text, maximum: 255
   validates_length_of :url, in: 11..255, message: "%{value} is not a valid url"
   validates_inclusion_of :category, in: CATEGORIES, message: "%{value} is not a valid category"
 
