06a327cce4066ff1d53fbe3df276766c9e9475ff
Will Fisher <will@teknofire.net>
2014-02-18 17:40:13 -0900

    will now cache pdf's locating while indexing them

--- app/models/link.rb
+++ app/models/link.rb
@@ -11,16 +11,52 @@ class Link < ActiveRecord::Base
     self.url.split(".").last.downcase == "pdf"
   end
 
+  # Pre: 
+  #   url is a valid pdf file location
+  # Post:
+  #   returns filename of the the localy cached file
+  def cached_pdf(url)
+    cache_dir = Rails.root.join('tmp/pdf_cache')
+    FileUtils.mkdir_p(cache_dir)
+    
+    cache_filename = cache_dir.join(url.gsub('http://', '').gsub(/[\/%]/, '_'))
+        
+    if File.size?(cache_filename).nil?
+      pbar = nil
+      opts = {
+        read_timeout: 1, 
+        content_length_proc: lambda {|t|
+          if t && 0 < t
+            pbar = ProgressBar.new(t)
+            #pbar.file_transfer_mode
+          end
+        },
+        progress_proc: lambda {|s| pbar.increment! s if pbar }
+      }
+      puts "Caching #{url}"
+      open(cache_filename, 'wb') do |output|
+        output << open(url, opts).read
+      end
+    end
+    return cache_filename
+  rescue => e
+    FileUtils.rm(cache_filename)
+    raise e
+  end
+
   def pdf_to_text
     return "" unless self.is_pdf?
+    
     pdf_text = ""
     begin      
-      io = open(self.url)
-      PDF::Reader.new(io).pages.each do |page|
-        pdf_text << page.text
+      pdf = PDF::Reader.new(cached_pdf(self.url))
+      [20, pdf.page_count].min.times do |i|
+        pdf_text << pdf.page(i+1).text
       end
-    rescue
+    rescue => e
       # don't die because of pdf or http error
+      puts self.url
+      puts "Error: " + e.message
     end
 
     return pdf_text
