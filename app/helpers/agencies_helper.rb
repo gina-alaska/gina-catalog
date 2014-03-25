0fa6f3c89521e144173ec83a1bd447c8fea914e5
grimm <jason@gina.alaska.edu>
2014-03-24 16:20:31 -0800

    Converted the agencies solr search to database search so that it will be faster.

--- app/helpers/agencies_helper.rb
+++ app/helpers/agencies_helper.rb
@@ -1,12 +1,9 @@
 module AgenciesHelper
   def collection_select_for_agencies(name, selected, special, html_opts = {})
-    search = Agency.search do
-      with :setup_ids, current_setup.self_and_descendants.pluck(:id)
-      paginate per_page:(5000)
-      order_by(:name, "asc")
-    end
+    setup_ids = current_setup.self_and_descendants.pluck(:id)
+    agencies = Agency.joins(:setups).where(setups: {id: setup_ids}).order(:name).uniq
 
-    options = options_from_collection_for_select(search.results, :id, :name_with_acronym, selected)
+    options = options_from_collection_for_select(agencies, :id, :name_with_acronym, selected)
     html_opts.merge!({ data: {placeholder: 'All Agencies', ui: 'select2'}, multiple: true, :include_blank => true })
     html_opts[:class] ||= 'span12'
     
