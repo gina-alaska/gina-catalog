7326088d860d61803fa3b840826cb0927a5c7802
Scott Macfarlane <scott@gina.alaska.edu>
2014-04-18 17:31:06 -0800

    Fix missing semicolon

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'will@gina.alaska.edu'
 license          'All rights reserved'
 description      'Installs/Configures glynx'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '2.0.1'
+version          '2.0.2'
 
 supports "centos", ">= 6.0"
 
@@ -13,8 +13,8 @@ depends "chruby"
 depends "user"
 depends "yum", '~> 3.0'
 depends "yum-epel"
-depends 'unicorn', '~> 1.0.0'
+depends 'unicorn', '~> 1.3.0'
 depends "gina", '~> 0.5.5'
-depends "gina-postgresql"
-depends "nginx"
+depends "gina-postgresql", '~> 0.4.0'
+depends "nginx", '~> 2.6.0'
 depends "sudo", '~> 2.5.2'
