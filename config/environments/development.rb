a48d037f2d12f5d2746dd091b8b6be7690936566
Will Fisher <will@teknofire.net>
2014-07-24 11:16:51 -0800

    use a single file, faster for the gvm environment

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ NSCatalog::Application.configure do
   config.assets.compress = false
   
   # Expands the lines which load the assets
-  config.assets.debug = true
+  config.assets.debug = false
 
   # Generate digests for assets URLs
   config.assets.digest = false
