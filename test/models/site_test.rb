eb57fb4e7ec64bf9f7450f6ed59a3e47f169a405
Grimm <jason@gina.alaska.edu>
2014-09-05 15:53:42 -0800

    Fixed site_test.rb.

--- test/models/site_test.rb
+++ test/models/site_test.rb
@@ -1,29 +1,10 @@
 require 'test_helper'
 
 class SiteTest < ActiveSupport::TestCase
-<<<<<<< HEAD
-  # test "the truth" do
-  #   assert true
-  # end
-  def setup
-  	@site = sites(:one)
-  end
-
-
-  test "sites should have urls" do
-  	assert !@site.urls.empty?
-	end
-
-	test "sites should have only one default url" do
-		@site.urls.create(url: 'test.com', default: true)
-		assert !@site.valid?, "Site came back as valid when it should be invalid"
-	end
-=======
   should validate_presence_of(:title)
   should validate_presence_of(:acronym)
   
   should have_many(:urls)
   
   should accept_nested_attributes_for(:urls).allow_destroy(true)
->>>>>>> 2dfccd1101e1c9cd3b6cb386e0fd36b7efae84a5
 end
