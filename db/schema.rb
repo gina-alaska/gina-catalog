a1b93d00b2f71951de191b0239c3b83f0a19261a
Grimm <jason@gina.alaska.edu>
2014-09-08 11:01:15 -0800

    Added address model, missed this one.

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140906003535) do
+ActiveRecord::Schema.define(version: 20140908184125) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
   enable_extension "postgis"
   enable_extension "postgis_topology"
+
+  create_table "addresses", force: true do |t|
+    t.string   "line1"
+    t.string   "line2"
+    t.string   "country"
+    t.string   "state"
+    t.string   "city"
+    t.string   "zipcode"
+    t.integer  "addressable_id"
+    t.string   "addressable_type"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "agency_contacts", force: true do |t|
     t.integer  "contact_id"
