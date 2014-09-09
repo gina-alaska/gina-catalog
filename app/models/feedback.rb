17c74ac263387fb4d09863a1a3fab8f81e0416b1
Grimm <jason@gina.alaska.edu>
2014-09-09 15:17:38 -0800

    Removed validations from key fields.

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,6 +1,4 @@
 class Feedback < ActiveRecord::Base
   validates :name, length: { maximum: 255 }
   validates :email, length: { maximum: 255 }
-  validates :user_id, numericality: { only_integer: true }
-  validates :site_id, numericality: { only_integer: true }
 end
--- test/models/feedback_test.rb
+++ test/models/feedback_test.rb
@@ -3,6 +3,4 @@ require 'test_helper'
 class FeedbackTest < ActiveSupport::TestCase
   should ensure_length_of(:name).is_at_most(255)
   should ensure_length_of(:email).is_at_most(255)
-  should validate_numericality_of(:user_id).only_integer
-  should validate_numericality_of(:site_id).only_integer
 end
