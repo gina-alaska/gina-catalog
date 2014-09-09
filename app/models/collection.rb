353d60def46f80a4bc2d96c2f0a55482682235c5
Grimm <jason@gina.alaska.edu>
2014-09-09 15:18:48 -0800

    Removed validations from key fields.

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,4 +1,3 @@
 class Collection < ActiveRecord::Base
   validates :name, length: { maximum: 255 }
-  validates :site_id, numericality: { only_integer: true }
 end
--- test/models/collection_test.rb
+++ test/models/collection_test.rb
@@ -2,5 +2,4 @@ require 'test_helper'
 
 class CollectionTest < ActiveSupport::TestCase
   should ensure_length_of(:name).is_at_most(255)
-  should validate_numericality_of(:site_id).only_integer
 end
