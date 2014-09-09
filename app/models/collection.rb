f26c5db93e33bfaf61fcb5fe13b84d280a10ac6b
Grimm <jason@gina.alaska.edu>
2014-09-09 12:52:25 -0800

    Added tests and validations to the collection model.

--- app/models/collection.rb
+++ app/models/collection.rb
@@ -1,2 +1,4 @@
 class Collection < ActiveRecord::Base
+  validates :name, length: { maximum: 255 }
+  validates :site_id, numericality: { only_integer: true }
 end
--- test/models/collection_test.rb
+++ test/models/collection_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class CollectionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should ensure_length_of(:name).is_at_most(255)
+  should validate_numericality_of(:site_id).only_integer
 end
