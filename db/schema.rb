d3bc73d6fdf1fb3b73b95e9b0c48b72b92cbc56d
Grimm <jason@gina.alaska.edu>
2014-09-09 12:17:03 -0800

    Created the aliases model.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140909011301) do
+ActiveRecord::Schema.define(version: 20140909201613) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,14 @@ ActiveRecord::Schema.define(version: 20140909011301) do
   create_table "agency_contacts", force: true do |t|
     t.integer  "contact_id"
     t.integer  "agency_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "aliases", force: true do |t|
+    t.string   "text"
+    t.integer  "aliasable_id"
+    t.string   "aliasable_type"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
