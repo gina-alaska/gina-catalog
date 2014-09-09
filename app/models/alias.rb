58aa90934e71dc1dabb286c1ccbd9e54f0bf8468
Grimm <jason@gina.alaska.edu>
2014-09-09 15:19:48 -0800

    Removed validations from key fields.

--- app/models/alias.rb
+++ app/models/alias.rb
@@ -1,5 +1,4 @@
 class Alias < ActiveRecord::Base
   validates :text, length: { maximum: 255 }
   validates :aliasable_type, length: { maximum: 255 }
-  validates :aliasable_id, numericality: { only_integer: true }
 end
--- test/models/alias_test.rb
+++ test/models/alias_test.rb
@@ -3,5 +3,4 @@ require 'test_helper'
 class AliasTest < ActiveSupport::TestCase
   should ensure_length_of(:text).is_at_most(255)
   should ensure_length_of(:aliasable_type).is_at_most(255)
-  should validate_numericality_of(:aliasable_id).only_integer
 end
