6b71bf9f3af83970e3b4ebb50a1980aa134beff4
Grimm <jason@gina.alaska.edu>
2014-09-09 14:22:30 -0800

    Added tests and validations to the feedback model.

--- app/models/feedback.rb
+++ app/models/feedback.rb
@@ -1,2 +1,6 @@
 class Feedback < ActiveRecord::Base
+  validates :name, length: { maximum: 255 }
+  validates :email, length: { maximum: 255 }
+  validates :user_id, numericality: { only_integer: true }
+  validates :site_id, numericality: { only_integer: true }
 end
