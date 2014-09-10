e5d4614487095c5fd314aa0fe03afb99757888a2
Grimm <jason@gina.alaska.edu>
2014-09-10 10:12:57 -0800

    Removed validations, not needed.

--- app/models/alias.rb
+++ app/models/alias.rb
@@ -1,4 +1,2 @@
 class Alias < ActiveRecord::Base
-  validates :text, length: { maximum: 255 }
-  validates :aliasable_type, length: { maximum: 255 }
 end
--- test/models/alias_test.rb
+++ test/models/alias_test.rb
@@ -1,6 +1,4 @@
 require 'test_helper'
 
 class AliasTest < ActiveSupport::TestCase
-  should ensure_length_of(:text).is_at_most(255)
-  should ensure_length_of(:aliasable_type).is_at_most(255)
 end
