679c17bcfca700efa3a20da178dbe97291874750
Grimm <jason@gina.alaska.edu>
2014-09-16 11:57:05 -0800

    Finished contacts testing setup.

--- test/controllers/manager/contacts_controller_test.rb
+++ test/controllers/manager/contacts_controller_test.rb
@@ -1,15 +1,22 @@
 require 'test_helper'
 
 class Manager::ContactsControllerTest < ActionController::TestCase
+  def setup
+    @contact = contacts(:one)
+    @user = users(:admin)
+    session[:user_id] = @user.id
+  end
+
   test "should get index" do
     get :index
     assert_response :success
+    assert_not_nil assigns(:contacts)
   end
 
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
+  #test "should get show" do
+  #  get :show
+  #  assert_response :success
+  #end
 
   test "should get new" do
     get :new
@@ -17,23 +24,25 @@ class Manager::ContactsControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
-    get :edit
+    get :edit, id: @contact.id
     assert_response :success
   end
 
   test "should get create" do
-    get :create
-    assert_response :success
+    post :create, contact: @contact.attributes
+    assert assigns(:contact).errors.empty?, assigns(:contact).errors.full_messages
+    assert_redirected_to manager_contacts_path
   end
 
   test "should get update" do
-    get :update
-    assert_response :success
+    put :update, id: @contact.id, contact: @contact.attributes
+    assert assigns(:contact).errors.empty?, assigns(:contact).errors.full_messages
+    assert_redirected_to manager_contacts_path
   end
 
   test "should get destroy" do
-    get :destroy
-    assert_response :success
+    delete :destroy, id: @contact.id
+    assert_redirected_to manager_contacts_path
   end
 
 end
