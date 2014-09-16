14d6d56b7f21670619eeae13368552fa01dad21e
Grimm <jason@gina.alaska.edu>
2014-09-16 15:20:04 -0800

    Generated the agencies controller and added routes.

---
+++ app/controllers/manager/agencies_controller.rb
@@ -0,0 +1,22 @@
+class Manager::AgenciesController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
 
   namespace :manager do
     resources :contacts
+    resources :agencies
   end
   
   # The priority is based upon order of creation: first created -> highest priority.
