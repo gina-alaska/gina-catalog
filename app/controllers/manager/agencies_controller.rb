c7271916b3c4d5ce675b4a1c4fa384fcdeb2f708
Grimm <jason@gina.alaska.edu>
2014-09-22 12:10:12 -0800

    Added create action for agencies, also fixed the index and row views.

--- app/controllers/manager/agencies_controller.rb
+++ app/controllers/manager/agencies_controller.rb
@@ -22,11 +22,29 @@ class Manager::AgenciesController < ApplicationController
   end
 
   def create
+    @agency = Agency.new(agency_params)
+    # TODO: make sure that agencies do not need to be associated with sites. 
+
+    respond_to do |format|
+      if @agency.save
+        flash[:success] = "Agency #{@agency.name} was successfully created."
+        format.html { redirect_to manager_agencies_path }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @agency.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  protected
+  
+  def agency_params
+    params.require(:agency).permit(:name, :acronym, :description, :category, :url, :active, :logo, aliases_attributes: [:text, :_destroy])
   end
 end
