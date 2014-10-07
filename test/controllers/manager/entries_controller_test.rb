8d51f4c1859ccea6c3f612af66a5ad19580c2730
Grimm <jason@gina.alaska.edu>
2014-10-07 10:36:47 -0800

    Added the controller tests for entries.

--- test/controllers/manager/entries_controller_test.rb
+++ test/controllers/manager/entries_controller_test.rb
@@ -1,15 +1,22 @@
 require 'test_helper'
 
 class Manager::EntriesControllerTest < ActionController::TestCase
+  def setup
+    @entry = entries(:one)
+    @user = users(:admin)
+    session[:user_id] = @user.id
+  end
+
   test "should get index" do
     get :index
     assert_response :success
+    assert_not_nil assigns(:entries)
   end
 
-  test "should get show" do
-    get :show
-    assert_response :success
-  end
+  #test "should get show" do
+  #  get :show
+  #  assert_response :success
+  #end
 
   test "should get new" do
     get :new
@@ -17,23 +24,31 @@ class Manager::EntriesControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
-    get :edit
+    get :edit, id: @entry.id
     assert_response :success
   end
 
   test "should get create" do
-    get :create
-    assert_response :success
+    assert_difference('Entry.count') do
+      post :create, entry: @entry.attributes
+      assert assigns(:entry).errors.empty?, assigns(:entry).errors.full_messages
+    end
+
+    assert_redirected_to manager_entries_path
   end
 
   test "should get update" do
-    get :update
-    assert_response :success
+    patch :update, id: @entry.id, entry: { name: 'Testing2' }
+    assert assigns(:entry).errors.empty?, assigns(:entry).errors.full_messages
+    assert_redirected_to manager_entries_path
   end
 
   test "should get destroy" do
-    get :destroy
-    assert_response :success
+    assert_difference('Entry.count', -1) do
+      delete :destroy, id: @entry.id
+    end
+
+    assert_redirected_to manager_entries_path
   end
 
 end
