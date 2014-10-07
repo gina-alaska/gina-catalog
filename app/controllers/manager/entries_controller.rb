18baad7afa32f0380afa4514a3bbcb380adf90d7
Grimm <jason@gina.alaska.edu>
2014-10-07 10:37:15 -0800

    Added controller methods and views for entries.

--- app/controllers/manager/entries_controller.rb
+++ app/controllers/manager/entries_controller.rb
@@ -1,4 +1,6 @@
 class Manager::EntriesController < ApplicationController
+  load_and_authorize_resource
+
   def index
   end
 
@@ -12,11 +14,43 @@ class Manager::EntriesController < ApplicationController
   end
 
   def create
+    respond_to do |format|
+      if @entry.save
+        flash[:success] = "Entry #{@entry.title} was successfully created."
+        format.html { redirect_to manager_entries_path }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @entry.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def update
+    respond_to do |format|
+      if @entry.update_attributes(entry_params)
+        flash[:success] = "Entry #{@entry.title} was successfully updated."
+        format.html { redirect_to manager_entries_path }
+        format.json { head :nocontent }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @entry.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @entry.destroy
+
+    respond_to do |format|
+      flash[:success] = "Entry #{@entry.title} was successfully deleted."
+      format.html { redirect_to manager_entries_path }
+      format.json { head :no_content }
+    end
+  end
+  
+  protected
+  
+  def entry_params
+    params.require(:entry).permit(:title, :description, :status, :start_date, :end_date)
   end
 end
