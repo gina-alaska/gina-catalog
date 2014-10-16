46a6b57f916a6d87901ec9636b6734827e578a09
Grimm <jason@gina.alaska.edu>
2014-10-16 14:22:56 -0800

    Create link table.

---
+++ db/migrate/20141016221917_create_links.rb
@@ -0,0 +1,15 @@
+class CreateLinks < ActiveRecord::Migration
+  def change
+    create_table :links do |t|
+      t.string :category
+      t.string :display_text
+      t.string :url
+      t.boolean :valid_link, default: true
+      t.date :last_checked_at
+      t.integer :asset_id
+      t.string :asset_type
+
+      t.timestamps
+    end
+  end
+end
