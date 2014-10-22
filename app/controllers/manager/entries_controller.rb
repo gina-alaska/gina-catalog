56d229bdf2c1778e25e2305322948c604f41a7bf
Grimm <jason@gina.alaska.edu>
2014-10-22 10:20:11 -0800

    Added the secondary Boolean field in the entry_contacts table, views, and controller.

--- app/controllers/manager/entries_controller.rb
+++ app/controllers/manager/entries_controller.rb
@@ -54,6 +54,6 @@ class Manager::EntriesController < ApplicationController
   protected
   
   def entry_params
-    params.require(:entry).permit(:title, :description, :status, :start_date, :end_date, :use_agreement_id, :request_contact_info, :require_contact_info, entry_contacts_attributes: [:id, :contact_id, :primary, :_destroy])
+    params.require(:entry).permit(:title, :description, :status, :start_date, :end_date, :use_agreement_id, :request_contact_info, :require_contact_info, entry_contacts_attributes: [:id, :contact_id, :primary, :secondary, :_destroy])
   end
 end
