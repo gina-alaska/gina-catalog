e26a8778fcd46f20e827c19585a56438c7ffd894
Jen Delamere <jen@gina.alaska.edu>
2014-11-03 14:38:45 -0900

    Added create,update,destroy actions.

--- app/controllers/admin/entry_types_controller.rb
+++ app/controllers/admin/entry_types_controller.rb
@@ -7,16 +7,42 @@ class Admin::EntryTypesController < ApplicationController
   def new
   end
 
-  def create
-  end
-
   def edit
   end
 
+  def create
+    respond_to do |format|
+      if @entry_type.save
+        flash[:success] = "Catalog type #{@entry_type.name} was created."
+        format.html { redirect_to admin_entry_types_path }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @entry_type.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def update
+    respond_to do |format|
+      if @entry_type.update_attributes(entry_type_params)
+        flash[:success] = "Catalog type #{@entry_type.name} was updated."
+        format.html { redirect_to admin_entry_types_path }
+        format.json { head :nocontent }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @entry_type.errors, status: :unprocessable_entity }
+      end
+    end
   end
 
   def destroy
+    @entry_type.destroy
+
+    respond_to do |format|
+      flash[:success] = "Catalog type #{@entry_type.name} was deleted."
+      format.html { redirect_to admin_entry_types_path }
+      format.json { head :no_content }
+    end
   end
 
   protected
