a73836949ba2d7559e7df5be88b16efd606789db
Jen Delamere <jen@gina.alaska.edu>
2014-11-05 11:12:08 -0900

    Update portal model, parallel site model.

--- app/models/portal.rb
+++ app/models/portal.rb
@@ -1,2 +1,42 @@
 class Portal < ActiveRecord::Base
+  acts_as_nested_set  
+  
+  has_many :urls, class_name: 'PortalUrl'
+  has_one :default_url, -> { where default: true }, class_name: 'PortalUrl'
+  
+  has_many :permissions
+  has_many :invitations
+  has_many :users, through: :permissions
+  
+#  has_many :entry_sites
+#  has_many :entries, through: :entry_sites
+  
+  scope :active, -> { }
+  
+  validates :title, presence: true
+  validates :acronym, presence: true
+  validate :single_default_url
+  
+  
+  accepts_nested_attributes_for :urls, allow_destroy: true, reject_if: :blank_url
+ 
+   # validate :single_default_url
+
+  def blank_url(attributed)
+    attributed['url'].blank?
+  end
+  
+  # def default_url
+  #   self.urls.default_url.first.url
+  # end
+  
+  def default_url_count
+    self.urls.inject(0) { |c,v| c+=1 if v.default; c }
+  end
+
+  def single_default_url
+    if default_url_count > 1
+      errors.add(:urls, "cannot have more than one default url")
+    end
+  end
 end
