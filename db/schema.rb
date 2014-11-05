0da62a541e6bc195206fcc6848e8c4fc54c04b0e
Jen Delamere <jen@gina.alaska.edu>
2014-11-05 11:24:22 -0900

    Added boolean owner to EntryPortals.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141105201330) do
+ActiveRecord::Schema.define(version: 20141105202310) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -139,6 +139,7 @@ ActiveRecord::Schema.define(version: 20141105201330) do
     t.integer  "entry_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "owner"
   end
 
   create_table "entry_sites", force: true do |t|
