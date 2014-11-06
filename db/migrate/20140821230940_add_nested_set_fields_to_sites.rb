cea4761d35db7c18526d9ee06a629d6ffc10f210
Jen Delamere <jen@gina.alaska.edu>
2014-11-05 15:13:18 -0900

    Removing now-obsolete site files.

--- db/migrate/20140821230940_add_nested_set_fields_to_sites.rb
+++
@@ -1,8 +0,0 @@
-class AddNestedSetFieldsToSites < ActiveRecord::Migration
-  def change
-    add_column :sites, :parent_id, :integer
-    add_column :sites, :lft, :integer
-    add_column :sites, :rgt, :integer
-    add_column :sites, :depth, :integer
-  end
-end
