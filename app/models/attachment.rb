9e28def710f55444c6278dd21134dcf5e5edd98c
Jen Delamere <jen@gina.alaska.edu>
2014-11-13 14:43:35 -0900

    Fill in attachment model;entry/attachment assocs.
    * Copy content from glynx-2.0 upload model into attachment model.
    * Add entry/attachment tests. More tests need to be added.

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,2 +1,24 @@
 class Attachment < ActiveRecord::Base
+  INTERACTIONS = [
+    'Previewable',
+    'Downloadable'
+  ]
+  
+  dragonfly_accessor :file
+
+  belongs_to :entry
+
+  scope :downloadable, -> { where(interaction: "Downloadable") }
+  scope :previewable, -> { where(interaction: "Previewable") }
+
+  before_create :create_uuid
+  
+  def create_uuid
+    self.uuid = UUIDTools::UUID.md5_create(UUIDTools::UUID_URL_NAMESPACE, self.file_uid).to_s
+  end
+  
+  def to_param
+    self.uuid
+  end
+    
 end
--- test/models/attachment_test.rb
+++ test/models/attachment_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class AttachmentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should ensure_length_of(:description).is_at_most(255)
+
+  should belong_to(:entries)
 end
