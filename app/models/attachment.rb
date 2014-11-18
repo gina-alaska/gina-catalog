6a7c377f8cc3e41f6f51dfd2c7f42025c39efd1b
Grimm <jason@gina.alaska.edu>
2014-11-17 17:58:24 -0900

    Temporary until we get the UUIDTools fixed.

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -3,7 +3,7 @@ class Attachment < ActiveRecord::Base
     'Previewable',
     'Downloadable'
   ]
-  
+
   dragonfly_accessor :file
 
   belongs_to :entry
@@ -11,14 +11,14 @@ class Attachment < ActiveRecord::Base
   scope :downloadable, -> { where(interaction: "Downloadable") }
   scope :previewable, -> { where(interaction: "Previewable") }
 
-  before_create :create_uuid
-  
+  #before_create :create_uuid
+
   def create_uuid
     self.uuid = UUIDTools::UUID.md5_create(UUIDTools::UUID_URL_NAMESPACE, self.file_uid).to_s
   end
-  
+
   def to_param
     self.uuid
   end
-    
+
 end
