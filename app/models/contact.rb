4c593bb3c13bf6751b95620cbd9a8a0404488e05
Jen Delamere <jen@gina.alaska.edu>
2014-12-17 21:00:12 -0900

    * Restrict delete of contacts
    * Use a before_destroy callback.
    * Contact can only be deleted if it is not associated with a catalog
    record.
    * A disabled button signifies that the manager cannot delete a contact.

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ActiveRecord::Base
+  before_destroy :check_if_deletable, prepend: true
+  
   validate :has_name_email_or_title
   validates :name, length: { maximum: 255 }
   validates :email, length: { maximum: 255 }
@@ -7,9 +9,10 @@ class Contact < ActiveRecord::Base
 
   has_many :agency_contacts
   has_many :agencies, through: :agency_contacts
+  
   has_many :entry_contacts
   has_many :entries, through: :entry_contacts
-
+  
   def has_name_email_or_title
   	if self.name.blank? and self.email.blank? and self.job_title.blank?
   		errors.add(:name, "must have either a name, email, or job title!")
@@ -17,4 +20,24 @@ class Contact < ActiveRecord::Base
   		errors.add(:job_title, "must have either a name, email, or job title!")
   	end
   end
+
+  def deletable?
+    self.entries.empty?
+  end
+  
+  def not_deletable_reason
+    if !self.entries.empty?
+      "belongs to one or more catalog records"
+    end
+  end
+
+  private 
+  
+  def check_if_deletable
+    if !deletable?
+      errors.add(:base, "Cannot delete contact, #{not_deletable_reason}")
+      false
+    end
+  end
+  
 end
