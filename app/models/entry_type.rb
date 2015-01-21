21991031d368ab7d5f6b49a459fea3de9453a080
Will Fisher <will@teknofire.net>
2015-01-20 17:55:45 -0900

    fixing missing character

--- app/models/entry_type.rb
+++ app/models/entry_type.rb
@@ -4,7 +4,7 @@ class EntryType < ActiveRecord::Base
   has_many :entries
 
   validates :name, presence: true
-  validates :name, length:  maximum: 255 }
+  validates :name, length: { maximum: 255 }
   validates :description, length: { maximum: 255 }
   validates :color, presence: true
   validates :color, length: { maximum: 255 }
