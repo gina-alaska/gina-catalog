98ddf2b8b80b157d37113a17b7b56ebac042421c
Grimm <jason@gina.alaska.edu>
2015-01-23 16:13:58 -0900

    Added entry, region active record associations and region methods.

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,2 +1,14 @@
 class Region < ActiveRecord::Base
+  validates :name, length: { maximum: 255 }
+  validates_uniqueness_of :name
+  
+  has_many :entry_regions
+  has_many :entries, through: :entry_regions
+  
+  set_rgeo_factory_for_column(:geom, RGeo::Geographic.simple_mercator_factory(:srid => 4326))
+
+  def self.intersects(wkt, srid=4326)
+    wkt = wkt.as_text if wkt.respond_to? :as_text
+    where("ST_Intersects(geom, ?::geometry)", "SRID=#{srid};#{wkt}")
+  end  
 end
