0e257f01aeef4c6b7e09a71336457c1c930b16d0
Jen Delamere <jen@gina.alaska.edu>
2015-01-26 14:33:45 -0900

    Change command (same task).

--- app/models/invitation.rb
+++ app/models/invitation.rb
@@ -4,9 +4,9 @@ class Invitation < ActiveRecord::Base
   
   accepts_nested_attributes_for :permission
   
-  validates_presence_of :name
-  validates_presence_of :email
-  
+  validates :name, presence: true
+  validates :email, presence: true  
+
   def to_param
     self.uuid
   end
