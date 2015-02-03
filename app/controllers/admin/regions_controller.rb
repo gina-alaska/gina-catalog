1e5bc9888b555bade677007d25faf141b1b5484a
Grimm <jason@gina.alaska.edu>
2015-02-03 14:51:13 -0900

    Initial setup of the regions controller.

--- app/controllers/admin/regions_controller.rb
+++ app/controllers/admin/regions_controller.rb
@@ -3,4 +3,34 @@ class Admin::RegionsController < ApplicationController
 
   def index
   end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def create
+    respond_to do |format|
+      if @region.save
+        flash[:success] = "Region #{@region.name} was created."
+        format.html { redirect_to admin_regions_path }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @region.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  protected
+  
+  def region_params
+    params.require(:region).permit(:name, :geojson)
+  end
 end
