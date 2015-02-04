57f18de1717f4b0aaf47be2e37f3c483aa2ec058
Grimm <jason@gina.alaska.edu>
2015-02-04 10:58:57 -0900

    Added rgeo factory for use in region maps.

--- app/models/region.rb
+++ app/models/region.rb
@@ -1,11 +1,16 @@
 class Region < ActiveRecord::Base
+  module Factories
+    GEO = RGeo::Geographic.simple_mercator_factory(srid: 4326)
+    PROJECTED = GEO.projection_factory
+  end
+
   validates :name, length: { maximum: 255 }
   validates_uniqueness_of :name
   
   has_many :entry_regions
   has_many :entries, through: :entry_regions
   
-  set_rgeo_factory_for_column(:geom, RGeo::Geographic.simple_mercator_factory(:srid => 4326))
+  set_rgeo_factory_for_column(:geom, Factories::PROJECTED)
 
   def self.intersects(wkt, srid=4326)
     wkt = wkt.as_text if wkt.respond_to? :as_text
@@ -17,6 +22,8 @@ class Region < ActiveRecord::Base
   end
 
   def from_geojson(data)
-    self.geom = RGeo::GeoJSON.decode(data, json_parser: :json).first.geometry
+    geojson_data = RGeo::GeoJSON.decode(data, json_parser: :json, geo_factory: Factories::GEO)
+    logger.info("#{geojson_data.first.geometry.srid}")
+    self.geom = geojson_data.first.geometry
   end
 end
