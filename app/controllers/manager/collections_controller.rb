d01fb4a45536b485298f47695a0b02aa7919fc9d
Jen Delamere <jen@gina.alaska.edu>
2015-02-04 11:53:41 -0900

    Moving search to collections controller.

--- app/controllers/manager/collections_controller.rb
+++ app/controllers/manager/collections_controller.rb
@@ -58,6 +58,18 @@ class Manager::CollectionsController < ApplicationController
     end
   end
 
+  def search
+    @collections = current_portal.collections.order(:name)
+
+    if params[:q].present?
+      @collections = @collections.where('name ilike ?', "%#{params[:q]}%")
+    end
+    
+    respond_to do |format|
+      format.json 
+    end
+  end
+  
   protected
 
   def collection_params
