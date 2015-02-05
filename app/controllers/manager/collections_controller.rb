28653209452b249a5bf86e25778467d00b51beb0
Jen Delamere <jen@gina.alaska.edu>
2015-02-05 11:51:15 -0900

    Remove search action.

--- app/controllers/manager/collections_controller.rb
+++ app/controllers/manager/collections_controller.rb
@@ -57,18 +57,6 @@ class Manager::CollectionsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
-  def search
-    @collections = current_portal.collections.order(:name)
-
-    if params[:q].present?
-      @collections = @collections.where('name ilike ?', "%#{params[:q]}%")
-    end
-    
-    respond_to do |format|
-      format.json 
-    end
-  end
   
   protected
 
