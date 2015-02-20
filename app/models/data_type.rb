6690cd3d0e1acc6834d47c221991b70b4c899d92
Jen Delamere <jen@gina.alaska.edu>
2015-02-20 14:40:55 -0900

    Add name/description to model & test.

--- app/models/data_type.rb
+++ app/models/data_type.rb
@@ -1,2 +1,10 @@
 class DataType < ActiveRecord::Base
+
+  validates :name, presence: true
+  validates :name, length: { maximum: 255 }
+  validates :description, length: { maximum: 255 } 
+
+  def to_s
+    name
+  end
 end
--- test/models/data_type_test.rb
+++ test/models/data_type_test.rb
@@ -1,13 +1,8 @@
 require "test_helper"
 
 class DataTypeTest < ActiveSupport::TestCase
+  should validate_presence_of(:name)
 
-  def data_type
-    @data_type ||= DataType.new
-  end
-
-  def test_valid
-    assert data_type.valid?
-  end
-
+  should validate_length_of(:name).is_at_most(255)
+  should validate_length_of(:description).is_at_most(255)
 end
