0bad637dadb293c019f869b256519a08db1bb953
Jen Delamere <jen@gina.alaska.edu>
2015-02-24 09:43:43 -0900

    rubocop -a corrections....

--- app/models/data_type.rb
+++ app/models/data_type.rb
@@ -1,8 +1,7 @@
 class DataType < ActiveRecord::Base
-
   validates :name, presence: true
   validates :name, length: { maximum: 255 }
-  validates :description, length: { maximum: 255 } 
+  validates :description, length: { maximum: 255 }
 
   def to_s
     name
--- test/models/data_type_test.rb
+++ test/models/data_type_test.rb
@@ -1,4 +1,4 @@
-require "test_helper"
+require 'test_helper'
 
 class DataTypeTest < ActiveSupport::TestCase
   should validate_presence_of(:name)
