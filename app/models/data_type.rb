3789db2cdd3f5533793c41b504e943d787817f4f
Jen Delamere <jen@gina.alaska.edu>
2015-02-25 11:40:38 -0900

    Add DataType and Entries associations

--- app/models/data_type.rb
+++ app/models/data_type.rb
@@ -1,4 +1,8 @@
 class DataType < ActiveRecord::Base
+  include EntryDependentConcerns
+
+  has_many :entries
+
   validates :name, presence: true
   validates :name, length: { maximum: 255 }
   validates :description, length: { maximum: 255 }
