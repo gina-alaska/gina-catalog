cdd1b121b499ad49f4eb8afb3700f4fea868c6da
Grimm <jason@gina.alaska.edu>
2015-02-27 10:19:33 -0900

    Moved regions controller test to api.

--- test/controllers/api/regions_controller_test.rb
+++ test/controllers/api/regions_controller_test.rb
@@ -1,6 +1,6 @@
 require 'test_helper'
 
-class RegionsControllerTest < ActionController::TestCase
+class Api::RegionsControllerTest < ActionController::TestCase
   test 'Should get index' do
     get :index, format: :json
     assert_response :success
