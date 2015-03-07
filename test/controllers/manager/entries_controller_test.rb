c32caa335ecdbc852dd4bdf9c529b41ce699ecbc
Will Fisher <will@gina.alaska.edu>
2015-03-06 16:21:05 -0900

    moving tests back to appropriate place

--- test/controllers/manager/entries_controller_test.rb
+++ test/controllers/manager/entries_controller_test.rb
@@ -6,14 +6,6 @@ class Manager::EntriesControllerTest < ActionController::TestCase
     login_user(:portal_admin)
   end
 
-  test 'should get index' do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:entries)
-    assert_not_nil assigns(:facets)
-    assert_not_nil assigns(:search_params)
-  end
-
   #  test "should get show" do
   #    get :show
   #    assert_response :success
