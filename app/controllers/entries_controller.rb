825796fd740a8046362448554e9b4b8f4408f10c
Will Fisher <will@gina.alaska.edu>
2015-03-06 15:46:39 -0900

    moving controller actions and views

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,6 +1,16 @@
 class EntriesController < ApplicationController
   load_and_authorize_resource
 
+  include EntriesControllerSearchConcerns
+
+  def index
+    respond_to do |format|
+      format.html { search(params[:page], params[:limit] || 20) }
+      format.geojson { search(params[:page], params[:limit] || 500) }
+      format.json
+    end
+  end
+
   def show
     respond_to do |format|
       format.html
