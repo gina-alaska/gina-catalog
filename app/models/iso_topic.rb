4c122950668547ea788b4285e6c3a0ae02538059
Jen Delamere <jen@gina.alaska.edu>
2015-04-01 16:32:12 -0800

    Add validations for length of strings; update fixtures.limit

--- app/models/iso_topic.rb
+++ app/models/iso_topic.rb
@@ -1,6 +1,10 @@
 class IsoTopic < ActiveRecord::Base
   self.table_name = 'iso_topic_categories'
-  
+ 
+  validates :name, length: { maximum: 50 }
+  validates :long_name, length: { maximum: 200 }  
+  validates :iso_theme_code, length: { maximum: 3 }  
+ 
   def long_name_with_code
     "#{iso_theme_code} :: #{name.humanize} - #{long_name}"
   end
