2d2b93514ce9854d032d3cbe187d9d1da4714908
Jen Delamere <jen@gina.alaska.edu>
2015-04-03 11:19:29 -0800

    Rubocop -a fixes....

--- app/models/iso_topic.rb
+++ app/models/iso_topic.rb
@@ -1,18 +1,18 @@
 class IsoTopic < ActiveRecord::Base
   self.table_name = 'iso_topic_categories'
- 
+
   validates :name, length: { maximum: 50 }
-  validates :long_name, length: { maximum: 200 }  
-  validates :iso_theme_code, length: { maximum: 3 }  
- 
+  validates :long_name, length: { maximum: 200 }
+  validates :iso_theme_code, length: { maximum: 3 }
+
   def long_name_with_code
     "#{iso_theme_code} :: #{name.humanize} - #{long_name}"
   end
-  
+
   def as_json(*opts)
     hash = super
     hash[:long_name_with_code] = long_name_with_code
-    
+
     hash
   end
 end
--- test/models/iso_topic_test.rb
+++ test/models/iso_topic_test.rb
@@ -1,9 +1,7 @@
-require "test_helper"
+require 'test_helper'
 
 class IsoTopicTest < ActiveSupport::TestCase
-
   should validate_length_of(:name).is_at_most(50)
   should validate_length_of(:long_name).is_at_most(200)
   should validate_length_of(:iso_theme_code).is_at_most(3)
-
 end
