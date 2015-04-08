8853614ff8193d08bb0ff2b5e5dfc17e449eb4d3
Grimm <jason@gina.alaska.edu>
2015-04-08 15:45:58 -0800

    Added map_layers model tests.

--- test/models/map_layer_test.rb
+++ test/models/map_layer_test.rb
@@ -2,16 +2,12 @@ require "test_helper"
 
 class MapLayerTest < ActiveSupport::TestCase
   should belong_to(:entry)
-  
-  validates :name, presence: true
-  validates :url, presence: true
 
-  def map_layer
-    @map_layer ||= MapLayer.new
-  end
-
-  def test_valid
-    assert map_layer.valid?
-  end
-
+  should validate_presence_of(:name)
+  should ensure_length_of(:name).is_at_most(255)
+  should validate_presence_of(:url)
+  should ensure_length_of(:url).is_at_most(255)
+  should ensure_length_of(:type).is_at_most(255)
+  should ensure_length_of(:layers).is_at_most(255)
+  should ensure_length_of(:projections).is_at_most(255)
 end
