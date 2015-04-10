19be892356dabd459491f206ac9fce59b78fe428
Grimm <jason@gina.alaska.edu>
2015-04-10 10:44:43 -0800

    Fixed test and fixture.

--- test/models/download_test.rb
+++ test/models/download_test.rb
@@ -2,7 +2,5 @@ require "test_helper"
 
 class DownloadTest < ActiveSupport::TestCase
   should belong_to(:attachment)
-  should belong_to(:entry)
-  should ensure_length_of(:type).is_at_most(255)
-
+  should have_one(:entry).through(:attachment)
 end
