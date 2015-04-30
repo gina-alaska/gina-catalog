6c47b3ca7f46a5a1539492734941a0fe08ed7feb
Jen Delamere <jen@gina.alaska.edu>
2015-04-30 10:38:06 -0800

    Add collections to import

--- lib/import.rb
+++ lib/import.rb
@@ -3,6 +3,7 @@ require 'import/base'
 require 'import/entry'
 require 'import/contact'
 require 'import/organization'
+require 'import/collection'
 
 module Import
   API_URL = Rails.application.secrets.glynx2_api || 'http://glynx2-api.127.0.0.1.xip.io'
---
+++ lib/import/collection.rb
@@ -0,0 +1,36 @@
+require 'import/base'
+
+module Import
+  class Collection < Base
+    SIMPLE_FIELDS = %w(
+      name description
+    )
+     
+    def self.fetch(catalog)
+      import = ::Import::Collection.new(Portal.first)
+      Client.paged_results 'Collections', Client.collections_url(catalog) do |record|
+        import.create(record)
+      end
+    end
+
+    def initialize(portal)
+      @portal = portal
+    end
+
+    def create(json = {})
+      import = ImportItem.collections.oid(json['id']).first_or_initialize
+      import.importable ||= ::Collection.new
+
+      add_simple_fields(SIMPLE_FIELDS, import.importable, json)
+
+      import.importable.portal = @portal
+
+      import.save
+      unless import.importable.save
+        puts "Error saving collection #{import.import_id}"
+        puts import.importable.errors.full_messages
+      end
+      import
+    end
+  end
+end
