3bb98ea5ed243ee7124a3d10914d67ee13d0c849
Grimm <jason@gina.alaska.edu>
2015-05-11 11:58:52 -0800

    Added leaflet options to geojson map layers.

--- app/models/geojson_layer.rb
+++ app/models/geojson_layer.rb
@@ -7,4 +7,11 @@ class GeojsonLayer < MapLayer
   def layer_type
     'GeoJSON'
   end
+
+  def leaflet_options
+    opts = super()
+    opts['geojson-options'] = 'custom-marker'
+    opts['cluster'] = 'true'
+    opts
+  end
 end
