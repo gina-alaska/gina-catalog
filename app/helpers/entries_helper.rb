517ad7d19b99731524cfe89349d9bca4aacbca1f
Jen Delamere <jen@gina.alaska.edu>
2015-05-17 20:10:30 -0800

    Update full record view to contain list of data types

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -2,4 +2,9 @@ module EntriesHelper
   def list_regions(entry)
     entry.regions.pluck(:name).join(', ')
   end
+
+  def list_data_types(entry)
+    entry.data_types.pluck(:name).join(', ')
+  end
 end
+
