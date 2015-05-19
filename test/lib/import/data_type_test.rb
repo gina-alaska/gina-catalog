f1c0b6700d107c5788ff990fe1034450dcb4de4f
Jen Delamere <jen@gina.alaska.edu>
2015-05-19 14:03:24 -0800

    Update tests

---
+++ test/lib/import/data_type_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+require 'import'
+
+class Import::DataTypeTest < ActiveSupport::TestCase
+  test 'should create valid data type' do
+    data_type_import = Import::DataType.new
+    import = data_type_import.create(
+      'id' => 3,
+      'name' => 'Database',
+      'description' => 'Database test',
+    )
+    assert import.importable.valid?, import.importable.errors.full_messages
+  end
+end
