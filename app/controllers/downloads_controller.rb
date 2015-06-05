1c3ec76d92e480cff22060b88764b0db1925691e
Grimm <jason@gina.alaska.edu>
2015-06-05 09:44:38 -0800

    Added global id and attachment logic, also moved download log method from old attachment controller.

--- app/controllers/downloads_controller.rb
+++ app/controllers/downloads_controller.rb
@@ -1,7 +1,30 @@
 class DownloadsController < ApplicationController
-  def index
-  end
+  ATTACHMENTS_TO_LOG = ['Public Download', 'Private Download']
 
   def show
+    @download = GlobalID::Locator.locate_signed params[:id], for: 'download'
+
+    log_download if @download.class == Attachment && ATTACHMENTS_TO_LOG.include?(@download.category)
+
+    respond_to do |format|
+      format.any { send_file @download.file.path, filename: @download.file.name }
+    end
+  end
+
+  def sds
+    if (@download = Attachment.where(uuid: params[:id]).first).nil?
+      # @download = Link.where() add later
+    end
+
+    respond_to do |format|
+      format.html { redirect_to download_path(@download.global_id) }
+    end
+  end
+
+  private
+
+  def log_download
+    download_log = DownloadLog.new(user_agent: request.user_agent, user: current_user, entry: @download.entry, file_name: @download.file_name, portal: current_portal)
+    download_log.save
   end
 end
