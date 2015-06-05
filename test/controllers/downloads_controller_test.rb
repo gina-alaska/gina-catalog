4f54106cf213c38a1926d1bc5f177fb3b9828493
Grimm <jason@gina.alaska.edu>
2015-06-05 09:45:57 -0800

    Added tests.

--- test/controllers/downloads_controller_test.rb
+++ test/controllers/downloads_controller_test.rb
@@ -1,14 +1,29 @@
 require "test_helper"
 
 class DownloadsControllerTest < ActionController::TestCase
-  def test_index
-    get :index
-    assert_response :success
+  test 'should create new download log for public attachment download' do
+    Attachment.any_instance.stubs(:file).returns(OpenStruct.new(path: 'foo', name: 'bar'))
+    @controller.stubs(:send_file).returns(true)
+
+    assert_difference('DownloadLog.count') do
+      get :show, id: attachments(:public_download).global_id
+    end
   end
 
-  def test_show
-    get :show
-    assert_response :success
+  test 'should create new download log for private attachment download' do
+    Attachment.any_instance.stubs(:file).returns(OpenStruct.new(path: 'foo', name: 'bar'))
+    @controller.stubs(:send_file).returns(true)
+
+    assert_difference('DownloadLog.count') do
+      get :show, id: attachments(:private_download).global_id
+    end
   end
 
+  test 'should handle sds request with redirect' do
+    Attachment.any_instance.stubs(:global_id).returns('global_id')
+    @attachment = attachments(:public_download)
+    get :sds, id: @attachment
+
+    assert_redirected_to download_path('global_id')
+  end
 end
--- test/helpers/downloads_helper_test.rb
+++ test/helpers/downloads_helper_test.rb
@@ -1,9 +1,4 @@
 require "test_helper"
 
 class DownloadsHelperTest < ActionView::TestCase
-
-  def test_sanity
-    flunk "Need real tests"
-  end
-
 end
