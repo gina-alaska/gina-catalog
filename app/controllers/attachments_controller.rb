8e2beef0add3a2980b58af74cf7c1c1667757325
Grimm <jason@gina.alaska.edu>
2015-06-24 16:39:40 -0800

    Re-added attachments controller.

---
+++ app/controllers/attachments_controller.rb
@@ -0,0 +1,18 @@
+class AttachmentsController < ApplicationController
+  ATTACHMENTS_TO_LOG = ['Public Download', 'Private Download']
+
+  def show
+    response.headers['Access-Control-Allow-Origin'] = '*'
+
+    @attachment = Attachment.where(uuid: params[:id]).first
+    if ATTACHMENTS_TO_LOG.include?(@attachment.category)
+      download_log = DownloadLog.new(user_agent: request.user_agent, user: current_user, entry: @attachment.entry, file_name: @attachment.file_name, portal: current_portal)
+      download_log.save
+    end
+
+    respond_to do |format|
+      # format.geojson { send_data @attachment.file.data }
+      format.any { send_file @attachment.file.path, filename: @attachment.file.name }
+    end
+  end
+end
