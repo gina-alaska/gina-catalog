ff2f79b493bd5d7c81dd2e18603933d2a585e12e
Grimm <jason@gina.alaska.edu>
2015-07-31 12:21:00 -0800

    Added activities to map layers.

--- app/models/entry_map_layer.rb
+++ app/models/entry_map_layer.rb
@@ -5,9 +5,14 @@ class EntryMapLayer < ActiveRecord::Base
   include PublicActivity::Model
 
   tracked :owner => proc {|controller, model| controller.send(:current_user)},
-          :entry_id => :entry_id 
+          :entry_id => :entry_id,
+          parameters: :activity_params
 
   def to_s
     map_layer.name
+  end
+
+  def activity_params
+    { map_layer: map_layer.to_global_id.to_s  }
   end
 end
