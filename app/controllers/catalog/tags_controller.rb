306d50f2b2979de485cb6b4d20f33d37635780e0
Grimm <jason@gina.alaska.edu>
2015-08-19 15:43:02 -0800

    Add pagination to tag manager index and show views.

--- app/controllers/catalog/tags_controller.rb
+++ app/controllers/catalog/tags_controller.rb
@@ -3,7 +3,7 @@ class Catalog::TagsController < ApplicationController
   before_action :load_tag, only: [:show, :remove]
 
   def index
-    @tags = current_portal.entries.tag_counts_on(:tags).order(:name)
+    @tags = current_portal.entries.tag_counts_on(:tags).order(:name).page(params[:page])
     if params[:q].present?
       @tags = @tags.where('name ilike ?', "%#{params[:q]}%")
     end
@@ -22,7 +22,7 @@ class Catalog::TagsController < ApplicationController
   end
 
   def show
-    @entries = current_portal.entries.tagged_with(@tag.name).order(:title) unless @tag.nil?
+    @entries = current_portal.entries.tagged_with(@tag.name).order(:title).page(params[:page]) unless @tag.nil?
 
     respond_to do |format|
       format.html
