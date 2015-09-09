e7b50b1dc34a05ec97e8ed6b7fdfaa7e13958e47
Grimm <jason@gina.alaska.edu>
2015-09-09 14:33:28 -0800

    Added friendly stuff to theme model.

--- app/models/cms/theme.rb
+++ app/models/cms/theme.rb
@@ -1,3 +1,14 @@
 class Cms::Theme < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :name, use: :slugged
+
   belongs_to :portal
+
+  def should_generate_new_friendly_id?
+    if !slug? || name_changed?
+      true
+    else
+      false
+    end
+  end
 end
