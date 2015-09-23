d776e544388621ac2276c2f721e14fd655b5ce9b
Will Fisher <will@gina.alaska.edu>
2015-09-23 12:10:44 -0800

    adding support for nested files directories

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,6 +1,8 @@
 if Rails.env.development?
-  Refile.cache = Refile::Backend::FileSystem.new("tmp", max_size: 50.megabytes)
-  Refile.store = Refile::Backend::FileSystem.new("uploads/cms")
+  require 'glynx/refile/file_system'
+  require 'glynx/refile/hasher'
+  Refile.cache = Glynx::Refile::FileSystem.new("tmp/uploads/cache", max_size: 50.megabytes, hasher: Glynx::Refile::DateHasher.new)
+  Refile.store = Glynx::Refile::FileSystem.new("uploads/cms", hasher: Glynx::Refile::DateHasher.new)
 elsif Rails.env.production?
   # do stuff for s3!
 end
