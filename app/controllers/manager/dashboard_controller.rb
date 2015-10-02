acd126f6cd034dffb69c98299042ec223e5ec7cd
Grimm <jason@gina.alaska.edu>
2015-10-02 15:27:25 -0800

    Started to rework index view.

--- app/controllers/manager/dashboard_controller.rb
+++ app/controllers/manager/dashboard_controller.rb
@@ -1,4 +1,12 @@
 class Manager::DashboardController < ManagerController
   def index
+    @user_count = current_portal.users.count
+    @entry_count = current_portal.entries.count
+    @download_count = current_portal.download_logs.count
+    @top_downloads = current_portal.download_logs
+  end
+
+  def downloads
+
   end
 end
