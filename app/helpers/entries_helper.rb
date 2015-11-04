56d0e4bb3d65b385bcc8034b5629fd96e14c2b39
Grimm <jason@gina.alaska.edu>
2015-11-03 15:44:39 -0900

    Added functions for check_visible_params and limit_select options.

--- app/helpers/entries_helper.rb
+++ app/helpers/entries_helper.rb
@@ -6,4 +6,12 @@ module EntriesHelper
   def list_data_types(entry)
     entry.data_types.pluck(:name).join(', ')
   end
+
+  def check_export_visible_params(name)
+    !params['visible'].present? or params['visible'][name] == '1'
+  end
+
+  def limit_select
+    [["Limit: 30", 30], ["Limit:100", 100], ["Limit: 500", 500], ["Limit: 1000", 1000], ["Limit: 2000", 2000], ["Limit: 5000", 5000]]
+  end
 end
