692864f0339121983dbc21866797a261d208acfd
Will Fisher <will@gina.alaska.edu>
2015-11-03 16:37:41 -0900

    git rid of the old code

--- app/models/concerns/mustache_concerns.rb
+++ app/models/concerns/mustache_concerns.rb
@@ -23,26 +23,9 @@ module MustacheConcerns
   end
 
   def render_context(portal, page = nil)
-    # context = OpenStruct.new(page.try(:mustache_context) || mustache_context)
-    # context.public = !page.try(:draft)
-    #
-    # context.page = page
-
-    # context.child_pages = map_mustache_safe(page.children, page) unless page.nil?
-    # context.parent_page = page.try(:parent).try(:mustache_context)
-    # context.root_page = page.try(:root).try(:mustache_context)
-
-    # context.portal = portal.mustache_context(page)
-    # context.snippet = ->(context, name) { portal.snippets.where(name: name).first.try(:render, page) }
-    # context.pages = map_mustache_safe(portal.pages.roots.visible, page)
-    # context.newest_entries = map_mustache_safe(portal.entries.active.newest, page)
-
-    # unless page.nil?
-    #   context.image_attachments = map_mustache_safe(page.cms_page_attachments.map(&:attachment), page)
-    # end
-    # context.latest_entries = map_mustache_safe(portal.entries.order(updated_at: :desc).limit(5), page)
     portal.readonly!
     data = OpenStruct.new({ portal: portal })
+
     unless page.nil?
       page.readonly!
       data.page = page
