ba9e55f858e39549edda8a88fe4c0f4236b62e63
Grimm <jason@gina.alaska.edu>
2015-11-11 17:08:51 -0900

    Added sitemap routes and redirect.

--- config/routes.rb
+++ config/routes.rb
@@ -13,6 +13,7 @@ Rails.application.routes.draw do
   get '/admin' => 'admin/dashboard#index', as: :admin
   get '/manager' => 'manager/dashboards#index', as: :manager
   get '/portal_not_found' => 'welcome#portal_not_found', as: :portal_not_found
+  get '/sitemap' => 'sitemaps#index'
 
   # Support legacy routes
   get 'catalogs/:id' => 'import_items#entries'
@@ -23,6 +24,7 @@ Rails.application.routes.draw do
   resources :sessions
   resources :memberships
   resources :users
+  resources :sitemaps
   resources :downloads, constraints: { id: /[^\/]+/ } do
     get :sds, on: :member
   end
