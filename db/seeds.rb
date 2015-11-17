910f22e660628c1518cd3aba9ba914b81e826210
Will Fisher <will@gina.alaska.edu>
2015-11-17 13:05:15 -0900

    fixing adding of default urls

--- db/seeds.rb
+++ db/seeds.rb
@@ -53,9 +53,14 @@ default_portal = Portal.where(
   contact_email: 'support@gina.alaska.edu'
 ).first_or_create
 
-default_portal.urls.where(url: 'catalog.192.168.222.225.xip.io', default: false).first_or_create
-default_portal.urls.where(url: 'catalog.127.0.0.1.xip.io', default: false).first_or_create
-default_portal.urls.where(url: 'portal.gina.alaska.edu', default: true).first_or_create
+if default_portal.new_record?
+  # Only do this if the portal doesn't exist.
+  # If we move the default url to another portal don't want to put
+  # it back here at some later point.
+  default_portal.urls.where(url: 'catalog.192.168.222.225.xip.io', default: false).first_or_create
+  default_portal.urls.where(url: 'catalog.127.0.0.1.xip.io', default: false).first_or_create
+  default_portal.urls.where(url: 'portal.gina.alaska.edu', default: true).first_or_create
+end
 
 Contact.where(name: 'Will Fisher', email: 'will@alaska.edu').first_or_create
 
