de311f4c4044427762d3594a21452d7477ecbfe5
Grimm <jason@gina.alaska.edu>
2015-12-02 11:07:47 -0900

    Added help route

--- config/routes.rb
+++ config/routes.rb
@@ -172,6 +172,9 @@ Rails.application.routes.draw do
     resources :users
   end
 
+  # Help system
+  get '/help' => 'help#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
