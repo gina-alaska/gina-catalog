bb5c65f1ec722a76821f535a8ceb2d2a619b078c
Grimm <jason@gina.alaska.edu>
2015-12-03 14:41:24 -0900

    Changed the help route to better handle subsections.

--- config/routes.rb
+++ config/routes.rb
@@ -174,7 +174,7 @@ Rails.application.routes.draw do
   end
 
   # Help system
-  get '/help' => 'help#index'
+  get 'help/:action(/:id)' => 'help', as: :help
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
