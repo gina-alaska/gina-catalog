ed4b488a624a7764c8b5507c4d4f40295cf1f6cd
Will Fisher <will@gina.alaska.edu>
2016-03-03 16:41:20 -0900

    fixing production refile storage dir

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,12 +1,18 @@
-if Rails.env.production?
-  # do stuff for s3!
-else
-  require 'glynx/refile/file_system'
-  require 'glynx/refile/hasher'
+# if Rails.env.production?
+#   # do stuff for s3!
+# else
+#   require 'glynx/refile/file_system'
+#   require 'glynx/refile/hasher'
+#
+#   # turn off automounter so we can do it ourselves before the cms page slug glob
+#   Refile.cache = Glynx::Refile::FileSystem.new("tmp/uploads/cache", max_size: 50.megabytes, hasher: Glynx::Refile::DateHasher.new)
+#   Refile.store = Glynx::Refile::FileSystem.new("uploads/cms", hasher: Glynx::Refile::DateHasher.new)
+# end
+require 'glynx/refile/file_system'
+require 'glynx/refile/hasher'
 
-  # turn off automounter so we can do it ourselves before the cms page slug glob
-  Refile.cache = Glynx::Refile::FileSystem.new("tmp/uploads/cache", max_size: 50.megabytes, hasher: Glynx::Refile::DateHasher.new)
-  Refile.store = Glynx::Refile::FileSystem.new("uploads/cms", hasher: Glynx::Refile::DateHasher.new)
-end
+# turn off automounter so we can do it ourselves before the cms page slug glob
+Refile.cache = Glynx::Refile::FileSystem.new("tmp/uploads/cache", max_size: 50.megabytes, hasher: Glynx::Refile::DateHasher.new)
+Refile.store = Glynx::Refile::FileSystem.new("uploads/cms", hasher: Glynx::Refile::DateHasher.new)
 
 Refile.automount = false
