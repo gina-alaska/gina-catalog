b33f276c30391398fe51214e5035d3d7ede3d63a
Grimm <jason@gina.alaska.edu>
2016-06-27 10:56:18 -0800

    Removed the parent portal in share record drop down list as the records are shared automatically.

--- app/helpers/catalog/entries_helper.rb
+++ app/helpers/catalog/entries_helper.rb
@@ -1,6 +1,6 @@
 module Catalog::EntriesHelper
   def shareable_portals
-    current_user.portals.where.not(id: current_portal.id).order(title: :asc).each_with_object([]) do |portal, portals|
+    current_user.portals.where.not(id: [current_portal.id, current_portal.parent.id]).order(title: :asc).each_with_object([]) do |portal, portals|
       portals << portal if current_user.role?(:data_entry, portal) || current_user.role?(:data_manager, portal)
     end
   end
