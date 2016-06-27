633319a92f15f1f100e6e4abf2f283177f8567a5
Grimm <jason@gina.alaska.edu>
2016-06-27 11:12:16 -0800

    Added check for existing parent portal.

--- app/helpers/catalog/entries_helper.rb
+++ app/helpers/catalog/entries_helper.rb
@@ -1,6 +1,9 @@
 module Catalog::EntriesHelper
   def shareable_portals
-    current_user.portals.where.not(id: [current_portal.id, current_portal.parent.id]).order(title: :asc).each_with_object([]) do |portal, portals|
+    portal_ids = [current_portal.id]
+    portal_ids += [current_portal.parent.id] if current_portal.parent.present?
+    
+    current_user.portals.where.not(id: portal_ids).order(title: :asc).each_with_object([]) do |portal, portals|
       portals << portal if current_user.role?(:data_entry, portal) || current_user.role?(:data_manager, portal)
     end
   end
