18aaf20154c4f481c71d7d88d3493a2ce92c8ac4
Grimm <jason@gina.alaska.edu>
2016-06-27 12:10:13 -0800

    Removed the code to exclude the parent portal.

--- app/helpers/catalog/entries_helper.rb
+++ app/helpers/catalog/entries_helper.rb
@@ -1,9 +1,6 @@
 module Catalog::EntriesHelper
   def shareable_portals
-    portal_ids = [current_portal.id]
-    portal_ids += [current_portal.parent.id] if current_portal.parent.present?
-    
-    current_user.portals.where.not(id: portal_ids).order(title: :asc).each_with_object([]) do |portal, portals|
+    current_user.portals.where.not(id: current_portal.id).order(title: :asc).each_with_object([]) do |portal, portals|
       portals << portal if current_user.role?(:data_entry, portal) || current_user.role?(:data_manager, portal)
     end
   end
