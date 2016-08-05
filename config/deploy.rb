84f6bddb16d85df3fad11d201ecb26a8c454dfb3
Will Fisher <will@gina.alaska.edu>
2016-08-04 17:03:54 -0800

    specify the entire task name for the restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -37,8 +37,9 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # set :keep_releases, 5
 
 namespace :deploy do
-  after :deploy, :restart
-  after :restart, :clear_cache do
+  after 'deploy:publishing', 'deploy:restart'
+
+  task :restart do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
       # Here we can do anything such as:
       # within release_path do
