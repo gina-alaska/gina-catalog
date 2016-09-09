98eb2c87d870a572f518b3325058899d72a2cbdf
Will Fisher <will@gina.alaska.edu>
2016-09-09 11:32:02 -0800

    get rid of the ES for handling small searches

--- app/controllers/api/iso_topics_controller.rb
+++ app/controllers/api/iso_topics_controller.rb
@@ -1,14 +1,7 @@
 class Api::IsoTopicsController < ApplicationController
   def index
-    @iso_topics = IsoTopic.all.order(:name)
-    @iso_topics = IsoTopic.search(
-      search_params,
-      fields: [
-        { name: :word_start },
-        { iso_theme_code: :word_start },
-        { long_name: :word_start }
-      ]
-    )
+    @iso_topics = IsoTopic.where('name ilike :query or iso_theme_code ilike :query or long_name ilike :query', query: "%#{search_params}%")
+    @iso_topics = @iso_topics.order(iso_theme_code: :asc).all
 
     respond_to do |format|
       format.json
@@ -18,6 +11,6 @@ class Api::IsoTopicsController < ApplicationController
   protected
 
   def search_params
-    params[:query].present? ? params[:query] : '*'
+    params[:q].present? ? params[:q] : ''
   end
 end
