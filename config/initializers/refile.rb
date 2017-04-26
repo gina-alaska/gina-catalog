7ac5416c47c8a5012373d78ce0d9874fb149eb31
Will Fisher <will@gina.alaska.edu>
2017-04-26 11:04:07 -0800

    update to specify path for instances

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -11,8 +11,9 @@
 require 'glynx/refile/file_system'
 require 'glynx/refile/hasher'
 
+refile_storage_path = ::File.join(Rails.application.secrets.glynx_storage_path, 'cms')
 # turn off automounter so we can do it ourselves before the cms page slug glob
 Refile.cache = Glynx::Refile::FileSystem.new("tmp/uploads/cache", max_size: 50.megabytes, hasher: Glynx::Refile::DateHasher.new)
-Refile.store = Glynx::Refile::FileSystem.new("uploads/cms", hasher: Glynx::Refile::DateHasher.new)
+Refile.store = Glynx::Refile::FileSystem.new(refile_storage_path, hasher: Glynx::Refile::DateHasher.new)
 
 Refile.automount = false
