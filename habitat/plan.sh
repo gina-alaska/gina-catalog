53dbbcbce979f1140168345e2441f36fdd4ee016
Will Fisher <will@alaska.edu>
2017-07-10 10:06:15 -0800

    initial attempt

--- habitat/plan.sh
+++ habitat/plan.sh
@@ -7,6 +7,7 @@ pkg_license=('MIT')
 pkg_bin_dirs=(bin)
 
 pkg_deps=(
+  core/gcc-libs
   core/cacerts
   uafgina/imagemagick
   uafgina/geos
@@ -17,15 +18,15 @@ pkg_scaffolding=uafgina/scaffolding-ruby
 
 pkg_expose=(9292)
 
-# if [[ ! "$(declare -p scaffolding_symlinked_files 2> /dev/null || true)" =~ "declare -a" ]]; then
-#   declare -g -a scaffolding_symlinked_files
-# fi
-# scaffolding_symlinked_files+=(config/puma.rb)
+if [[ ! "$(declare -p scaffolding_symlinked_files 2> /dev/null || true)" =~ "declare -a" ]]; then
+  declare -g -a scaffolding_symlinked_files
+fi
+scaffolding_symlinked_files+=(config/puma.rb)
+scaffolding_symlinked_files+=(config/secrets.yml)
 
 do_build() {
   export SSL_CERT_FILE="$(pkg_path_for core/cacerts)/ssl/certs/cacert.pem"
-  build_line "Setting rgeo bundle config"
-  _bundle config build.rgeo "--with-geos-dir=$(pkg_path_for geos) --with-proj-dir=$(pkg_path_for geos)"
+  export BUNDLE_BUILD__RGEO="--with-geos-dir=$(pkg_path_for geos) --with-proj-dir=$(pkg_path_for geos)"
 
   do_default_build
 
