c79be2324bc0e5794bf54895c15fe76aea83a101
Will Fisher <will@alaska.edu>
2017-07-26 17:15:27 -0800

    clean up ability file

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,109 +1,85 @@
 class Ability
   include CanCan::Ability
 
-  def initialize(user, current_portal, controller_namespace)
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
+  def initialize(user, portal, _controller_namespace)
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
 
+    @portal = portal
     user ||= User.new
+    @user = user
 
+    setup_global_permissions
+    setup_global_admin_permissions if current_user.global_admin?
+    setup_portal_permissions
+  end
+
+  def setup_portal_permissions
+    setup_cms_manager_permissions     if current_user.role?(:cms_manager, current_portal)
+    setup_data_entry_permissions      if current_user.role?(:data_manager, current_portal) || current_user.role?(:data_entry, current_portal)
+    setup_data_manager_permissions    if current_user.role?(:data_manager, current_portal)
+    setup_portal_manager_permissions  if current_user.role?(:portal_manager, current_portal)
+  end
+
+  def current_portal
+    @portal
+  end
+
+  def current_user
+    @user
+  end
+
+  def setup_global_permissions
     can %i[read map], Entry, &:published?
     cannot %i[read map], Entry, published_at: nil
 
     can :read, Attachment
-    can :read, :dashboard
     cannot :read, Attachment, category: 'Private Download'
     can :exports, Entry
+    can :accept, Invitation unless current_user.new_record?
+  end
 
-    can :accept, Invitation unless user.new_record?
+  def setup_global_admin_permissions
+    can :view_admin, :menu
+    can :manage, [User, EntryType, Region, DataType, IsoTopic, Portal]
+  end
 
-    can :view_admin, :menu if user.global_admin?
+  def setup_portal_manager_permissions
+    can :view_portal, :menu
+    can :manage, [Permission, Invitation], portal_id: current_portal.id
+    can %i[read update], Portal, id: current_portal.id
+  end
 
-    can :view_cms, :menu if user.role?(:cms_manager, current_portal)
+  def setup_cms_manager_permissions
+    can :view_cms, :menu
+    can :read, :dashboard
+    can :manage, [Cms::Layout, Cms::Page, Cms::Snippet, Cms::Theme, Cms::Attachment], portal_id: current_portal.id
+  end
 
-    if user.role?(:data_entry, current_portal) || user.role?(:data_manager, current_portal)
-      can :view_catalog, :menu
+  def setup_data_entry_permissions
+    can :view_catalog, :menu
+
+    can %i[read preview], Attachment do |attachment|
+      attachment.entry.new_record? || attachment.entry.owner_portal = current_portal
     end
 
-    if user.global_admin?
-      can :manage, [User, EntryType, Region, DataType, IsoTopic]
-    end
-
-    if user.role?(:cms_manager, current_portal)
-      can :manage, [Cms::Layout, Cms::Page, Cms::Snippet, Cms::Theme, Cms::Attachment], portal_id: current_portal.id
-    end
-
-    if user.role?(:data_entry, current_portal)
-      can %i[read preview], Attachment do |attachment|
-        attachment.entry.new_record? || attachment.entry.owner_portal = current_portal
-      end
-
-      can :manage, [Organization, Contact, MapLayer]
-      can :manage, [UseAgreement, Collection], portal_id: current_portal.id
-      can %i[read map read_unpublished], Entry do |entry|
-        entry.new_record? || current_portal.self_and_ancestors.include?(entry.owner_portal)
-      end
-      can %i[create update archive], Entry do |entry|
-        entry.new_record? || entry.owner_portal == current_portal
-      end
-      can %i[downloads links], :dashboard
-    end
-
-    if user.role?(:data_manager, current_portal)
-      can %i[read preview], Attachment do |attachment|
-        attachment.entry.new_record? || attachment.entry.owner_portal = current_portal
-      end
-
-      can :manage, :tag
-      can :manage, [Organization, Contact, MapLayer]
-      can :manage, [UseAgreement, Collection], portal_id: current_portal.id
-
-      can %i[read map read_unpublished], Entry do |entry|
-        entry.new_record? || current_portal.self_and_ancestors.include?(entry.owner_portal)
-      end
-      can %i[create update destroy publish unpublish archive unarchive], Entry do |entry|
-        entry.new_record? || entry.owner_portal == current_portal
-      end
-      can %i[downloads links], :dashboard
-    end
-
-    if user.role?(:portal_manager, current_portal)
-      can :manage, [Permission, Invitation], portal_id: current_portal.id
-    end
-
-    case controller_namespace
-    when 'admin'
-      can :manage, Portal if user.global_admin?
-    else
-      if user.role?(:portal_manager, current_portal)
-        can %i[read update], Portal, id: current_portal.id
-      end
-    end
-
+    can :manage, [Organization, Contact, MapLayer]
+    can :manage, [UseAgreement, Collection], portal_id: current_portal.id
     cannot :update, UseAgreement, &:archived?
+
+    can %i[read map read_unpublished], Entry do |entry|
+      entry.new_record? || current_portal.self_and_ancestors.include?(entry.owner_portal)
+    end
+    can %i[create update archive], Entry do |entry|
+      entry.new_record? || entry.owner_portal == current_portal
+    end
+    can %i[downloads links], :dashboard
+  end
+
+  def setup_data_manager_permissions
+    can :manage, :tag
+    can %i[destroy publish unpublish unarchive], Entry do |entry|
+      entry.new_record? || entry.owner_portal == current_portal
+    end
   end
 end
