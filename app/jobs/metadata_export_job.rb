aa88a807822ea861c7b180c59fd16e38d76fa924
grimm <jason@gina.alaska.edu>
2017-07-27 17:23:12 -0800

    Added translator for converting json into adiwg format

--- app/jobs/metadata_export_job.rb
+++ app/jobs/metadata_export_job.rb
@@ -2,21 +2,18 @@ class MetadataExportJob < ActiveJob::Base
   queue_as :default
 
   include Rails.application.routes.url_helpers
-
+  require 'adiwg/mdtranslator'
     
   def perform(id)
     # Do something later
     uri = URI(api_adiwg_url(id: id))
     response = Net::HTTP.get_response(uri)
-    json_data = response.body
-    
+    metadata = ADIWG::Mdtranslator.translate(file: response.body, reader: 'mdJson')
+    puts metadata
   end
   
   protected
   def default_url_options
     {host: Rails.application.secrets.adiwg_host}
   end
-
-
 end
-
