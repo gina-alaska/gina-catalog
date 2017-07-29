3cf2cbf48cd2fab29fb3c19b9098018113b0880e
grimm <jason@gina.alaska.edu>
2017-07-28 16:39:11 -0800

    Add uuid field to links model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151208222758) do
+ActiveRecord::Schema.define(version: 20170728231620) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -401,6 +401,7 @@ ActiveRecord::Schema.define(version: 20151208222758) do
     t.date     "last_checked_at"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "uuid"
   end
 
   create_table "map_layers", force: :cascade do |t|
