95648ecf5efd5cc90b2b9916f755ed5752ba689d
grimm <jason@gina.alaska.edu>
2017-08-15 18:02:13 -0800

    Test fixes

--- test/controllers/catalog/entry_exports_controller_test.rb
+++ test/controllers/catalog/entry_exports_controller_test.rb
@@ -1,20 +1,9 @@
 require "test_helper"
 
-class EntryExportsControllerTest < ActionController::TestCase
+class Catalog::EntryExportsControllerTest < ActionController::TestCase
 
   def entry_export
     @entry_export ||= entry_exports :one
-  end
-
-  def test_index
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:entry_exports)
-  end
-
-  def test_new
-    get :new
-    assert_response :success
   end
 
   def test_create
@@ -30,21 +19,7 @@ class EntryExportsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  def test_edit
-    get :edit, id: entry_export
-    assert_response :success
-  end
-
   def test_update
     put :update, id: entry_export, entry_export: { collections: entry_export.collections, contacts: entry_export.contacts, data: entry_export.data, description: entry_export.description, description_chars: entry_export.description_chars, format_type: entry_export.format_type, info: entry_export.info, iso: entry_export.iso, limit: entry_export.limit, links: entry_export.links, location: entry_export.location, organizations: entry_export.organizations, serialized_search: entry_export.serialized_search, tags: entry_export.tags, title: entry_export.title, url: entry_export.url }
-    assert_redirected_to entry_export_path(assigns(:entry_export))
-  end
-
-  def test_destroy
-    assert_difference('EntryExport.count', -1) do
-      delete :destroy, id: entry_export
-    end
-
-    assert_redirected_to entry_exports_path
   end
 end
