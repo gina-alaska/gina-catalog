d1e9e1bd06227dc07faf262edc478dbbf5037e92
grimm <jason@gina.alaska.edu>
2018-08-16 13:46:37 -0800

    Forcing portal check for collections

--- app/controllers/concerns/entries_controller_search_concerns.rb
+++ app/controllers/concerns/entries_controller_search_concerns.rb
@@ -33,7 +33,11 @@ module EntriesControllerSearchConcerns
 
       facets = elastic_facets['buckets'].each_with_object([]) do |f, memo|
         if !model.nil?
-          facet_record = model.where(term_field => f['key'], portal_id: @portal).first
+          if facet_name == 'collection_ids'
+            facet_record = model.where(term_field => f['key'], :portal_id => @portal.id).first
+          else
+            facet_record = model.where(term_field => f['key']).first
+          end
           f['display_name'] = facet_record.try(display_field)
           f['hidden'] = facet_record.try(:hidden?)
         else
