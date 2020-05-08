b8c8bde1aa5130eeea20c382f219f5a26ce3dfcf
grimm <jason@gina.alaska.edu>
2020-05-08 13:02:38 -0800

    Escape all quotes.

--- app/helpers/cms_helper.rb
+++ app/helpers/cms_helper.rb
@@ -2,8 +2,8 @@ module CmsHelper
   def portal_analytics(tracking_id, namespace = 'default')
     return if tracking_id.blank?
     <<~EOJS
-      ga('create', '#{tracking_id}', 'auto', '#{namespace}');
-      ga('#{namespace}.send', 'pageview');
+      ga(\\'create\\', \\'#{tracking_id}\\', \\'auto\\', \\'#{namespace}\\');
+      ga(\\'#{namespace}.send\\', \\'pageview\\');
     EOJS
   end
 end
